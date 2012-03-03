942e74ec1c931579e7711c6080b265cac51cf6f6
Colby Dillion <colby.dillion@gmail.com>
2012-03-02 22:57:01 -0600

    update implementation version

--- DICOM/DicomImplementation.cs
+++ DICOM/DicomImplementation.cs
@@ -6,6 +6,6 @@ using System.Text;
 namespace Dicom {
 	public static class DicomImplementation {
 		public static DicomUID ClassUID = new DicomUID("1.3.6.1.4.1.30071.8", "Implementation Class UID", DicomUidType.Unknown);
-		public static string Version = "DICOM for .NET 4.0";
+		public static string Version = "fo-dicom 0.5";
 	}
 }
