f988679561422814b6fe2ff07660d75b706a2bda
Colby Dillion <colby.dillion@gmail.com>
2012-03-08 12:18:13 -0600

    create distribution package script

---
+++ Create Distribution Package.bat
@@ -0,0 +1,6 @@
+rmdir Distribute\ /S /Q
+mkdir Distribute\
+copy "DICOM\bin\Release\Dicom.dll" Distribute\ /Y
+copy "DICOM\bin\Release\Dicom.xml" Distribute\ /Y
+copy "DICOM [Native]\Release\Dicom.Native.dll" Distribute\ /Y
+copy "DICOM [Native]\Release\Dicom.Native64.dll" Distribute\ /Y
