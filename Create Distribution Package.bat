239cf054392aa809fcc12420ede1d7329edbd0bb
Colby Dillion <colby.dillion@gmail.com>
2012-06-22 15:48:35 -0500

    add license and readme files to distribution

--- Create Distribution Package.bat
+++ Create Distribution Package.bat
@@ -4,3 +4,5 @@ copy "DICOM\bin\Release\Dicom.dll" Distribute\ /Y
 copy "DICOM\bin\Release\Dicom.xml" Distribute\ /Y
 copy "DICOM [Native]\Release\Dicom.Native.dll" Distribute\ /Y
 copy "DICOM [Native]\Release\Dicom.Native64.dll" Distribute\ /Y
+copy License.txt Distribute\ /Y
+copy README.md Distribute\ /Y
