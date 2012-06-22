405dc892ece249eb3081b92beb3fdb740168b64e
Colby Dillion <colby.dillion@gmail.com>
2012-06-22 15:34:52 -0500

    update assembly info

--- DICOM/Properties/AssemblyInfo.cs
+++ DICOM/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DICOM for .NET 4.0")]
+[assembly: AssemblyTitle("DICOM for .NET")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Fellow Oak")]
-[assembly: AssemblyProduct("DICOM for .NET 4.0")]
+[assembly: AssemblyProduct("DICOM for .NET")]
 [assembly: AssemblyCopyright("Copyright © Fellow Oak, Inc. 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -31,4 +31,4 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.1.*")]
