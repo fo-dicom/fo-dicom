8ce68d9ef0f8653056d3dccd96b07593eb39772f
Colby Dillion <colby.dillion@gmail.com>
2012-07-22 17:24:15 -0500

    DICOM Dump example project

---
+++ Examples/DICOM Dump/MainForm.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace DICOM_Dump {
+	partial class MainForm {
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing) {
+			if (disposing && (components != null)) {
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent() {
+			this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+			this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.lvDicom = new System.Windows.Forms.ListView();
+			this.columnHeaderTag = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.columnHeaderVR = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.columnHeaderLength = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.columnHeaderValue = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.menuStrip1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// menuStrip1
+			// 
+			this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+			this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+			this.menuStrip1.Name = "menuStrip1";
+			this.menuStrip1.Size = new System.Drawing.Size(792, 24);
+			this.menuStrip1.TabIndex = 0;
+			this.menuStrip1.Text = "menuStrip1";
+			// 
+			// fileToolStripMenuItem
+			// 
+			this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem});
+			this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+			this.fileToolStripMenuItem.Size = new System.Drawing.Size(35, 20);
+			this.fileToolStripMenuItem.Text = "&File";
+			// 
+			// openToolStripMenuItem
+			// 
+			this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+			this.openToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+			this.openToolStripMenuItem.Text = "&Open";
+			this.openToolStripMenuItem.Click += new System.EventHandler(this.OnClickOpen);
+			// 
+			// lvDicom
+			// 
+			this.lvDicom.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeaderTag,
+            this.columnHeaderVR,
+            this.columnHeaderLength,
+            this.columnHeaderValue});
+			this.lvDicom.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.lvDicom.FullRowSelect = true;
+			this.lvDicom.Location = new System.Drawing.Point(0, 24);
+			this.lvDicom.MultiSelect = false;
+			this.lvDicom.Name = "lvDicom";
+			this.lvDicom.Size = new System.Drawing.Size(792, 549);
+			this.lvDicom.TabIndex = 1;
+			this.lvDicom.UseCompatibleStateImageBehavior = false;
+			this.lvDicom.View = System.Windows.Forms.View.Details;
+			// 
+			// columnHeaderTag
+			// 
+			this.columnHeaderTag.Text = "Tag";
+			this.columnHeaderTag.Width = 230;
+			// 
+			// columnHeaderVR
+			// 
+			this.columnHeaderVR.Text = "VR";
+			this.columnHeaderVR.Width = 40;
+			// 
+			// columnHeaderLength
+			// 
+			this.columnHeaderLength.Text = "Length";
+			// 
+			// columnHeaderValue
+			// 
+			this.columnHeaderValue.Text = "Value";
+			this.columnHeaderValue.Width = 440;
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(792, 573);
+			this.Controls.Add(this.lvDicom);
+			this.Controls.Add(this.menuStrip1);
+			this.MainMenuStrip = this.menuStrip1;
+			this.Name = "MainForm";
+			this.Text = "DICOM Dump";
+			this.menuStrip1.ResumeLayout(false);
+			this.menuStrip1.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.MenuStrip menuStrip1;
+		private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+		private System.Windows.Forms.ListView lvDicom;
+		private System.Windows.Forms.ColumnHeader columnHeaderTag;
+		private System.Windows.Forms.ColumnHeader columnHeaderVR;
+		private System.Windows.Forms.ColumnHeader columnHeaderLength;
+		private System.Windows.Forms.ColumnHeader columnHeaderValue;
+	}
+}
+
---
+++ Examples/DICOM Dump/MainForm.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using Dicom;
+using Dicom.IO.Buffer;
+
+namespace DICOM_Dump {
+	public partial class MainForm : Form {
+		public MainForm() {
+			InitializeComponent();
+		}
+
+		private void Reset() {
+			lvDicom.Items.Clear();
+		}
+
+		private delegate void AddItemDelegate(string tag, string vr, string length, string value);
+
+		private void AddItem(string tag, string vr, string length, string value) {
+			if (InvokeRequired) {
+			    BeginInvoke(new AddItemDelegate(AddItem), tag, vr, length, value);
+			    return;
+			}
+
+			var lvi = lvDicom.Items.Add(tag);
+			lvi.SubItems.Add(vr);
+			lvi.SubItems.Add(length);
+			lvi.SubItems.Add(value);
+		}
+
+		private void OnClickOpen(object sender, EventArgs e) {
+			var ofd = new OpenFileDialog();
+			if (ofd.ShowDialog() == DialogResult.Cancel)
+				return;
+
+			try {
+				lvDicom.BeginUpdate();
+
+				Reset();
+
+				var file = DicomFile.Open(ofd.FileName);
+				new DicomDatasetWalker(file.FileMetaInfo).Walk(new DumpWalker(this));
+				new DicomDatasetWalker(file.Dataset).Walk(new DumpWalker(this));
+			} catch (Exception ex) {
+				MessageBox.Show(this, "Exception while loading DICOM file: " + ex.Message, "Error loading DICOM file", MessageBoxButtons.OK, MessageBoxIcon.Error);
+			} finally {
+				lvDicom.EndUpdate();
+			}
+		}
+
+		private class DumpWalker : IDicomDatasetWalker {
+			int _level = 0;
+
+			public DumpWalker(MainForm form) {
+				Form = form;
+				Level = 0;
+			}
+
+			public MainForm Form {
+				get;
+				set;
+			}
+
+			public int Level {
+				get { return _level; }
+				set {
+					_level = value;
+					Indent = String.Empty;
+					for (int i = 0; i < _level; i++)
+						Indent += "    ";
+				}
+			}
+
+			private string Indent {
+				get;
+				set;
+			}
+
+			public void OnBeginWalk(DicomDatasetWalker walker, DicomDatasetWalkerCallback callback) {
+			}
+
+			public bool OnElement(DicomElement element) {
+				var tag = String.Format("{0}{1}  {2}", Indent, element.Tag.ToString().ToUpper(), element.Tag.DictionaryEntry.Name);
+
+				string value = "<large value not displayed>";
+				if (element.Length <= 2048)
+					value = String.Join("\\", element.Get<string[]>());
+
+				Form.AddItem(tag, 
+					element.ValueRepresentation.Code, 
+					element.Length.ToString(), 
+					value);
+				return true;
+			}
+
+			public bool OnBeginSequence(DicomSequence sequence) {
+				var tag = String.Format("{0}{1}  {2}", Indent, sequence.Tag.ToString().ToUpper(), sequence.Tag.DictionaryEntry.Name);
+
+				Form.AddItem(tag, "SQ", String.Empty, String.Empty);
+
+				Level++;
+				return true;
+			}
+
+			public bool OnBeginSequenceItem(DicomDataset dataset) {
+				var tag = String.Format("{0}Sequence Item:", Indent);
+
+				Form.AddItem(tag, String.Empty, String.Empty, String.Empty);
+
+				Level++;
+				return true;
+			}
+
+			public bool OnEndSequenceItem() {
+				Level--;
+				return true;
+			}
+
+			public bool OnEndSequence() {
+				Level--;
+				return true;
+			}
+
+			public bool OnBeginFragment(DicomFragmentSequence fragment) {
+				var tag = String.Format("{0}{1}  {2}", Indent, fragment.Tag.ToString().ToUpper(), fragment.Tag.DictionaryEntry.Name);
+
+				Form.AddItem(tag, fragment.ValueRepresentation.Code, String.Empty, String.Empty);
+
+				Level++;
+				return true;
+			}
+
+			public bool OnFragmentItem(IByteBuffer item) {
+				var tag = String.Format("{0}Fragment", Indent);
+
+				Form.AddItem(tag, String.Empty, item.Size.ToString(), String.Empty);
+				return true;
+			}
+
+			public bool OnEndFragment() {
+				Level--;
+				return true;
+			}
+
+			public void OnEndWalk() {
+			}
+		}
+	}
+}
