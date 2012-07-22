e379c03e555bb692e202bb523edb9faecdc263eb
Colby Dillion <colby.dillion@gmail.com>
2012-07-22 17:32:35 -0500

    include examples in distribution

--- Create Distribution Package.bat
+++ Create Distribution Package.bat
@@ -1,5 +1,6 @@
 rmdir Distribute\ /S /Q
 mkdir Distribute\
+mkdir Distribute\Examples
 copy "DICOM\bin\Release\Dicom.dll" Distribute\ /Y
 copy "DICOM\bin\Release\Dicom.xml" Distribute\ /Y
 copy "DICOM [Native]\Release\Dicom.Native.dll" Distribute\ /Y
@@ -7,3 +8,5 @@ copy "DICOM [Native]\Release\Dicom.Native64.dll" Distribute\ /Y
 copy License.txt Distribute\ /Y
 copy README.md Distribute\ /Y
 copy ChangeLog.md Distribute\ /Y
+copy "Examples\C-Store SCP\bin\Release\Dicom.CStoreSCP.exe" Distribute\Examples /Y
+copy "Examples\DICOM Dump\bin\Release\Dicom.Dump.exe" Distribute\Examples /Y
