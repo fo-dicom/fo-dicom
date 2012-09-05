4b5f5c23449461fb2ca6a549651a183872db9d91
Colby Dillion <colby.dillion@gmail.com>
2012-09-05 16:07:32 -0500

    load implementation version from assembly info

--- DICOM/DicomImplementation.cs
+++ DICOM/DicomImplementation.cs
@@ -1,11 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 
 namespace Dicom {
 	public static class DicomImplementation {
 		public static DicomUID ClassUID = new DicomUID("1.3.6.1.4.1.30071.8", "Implementation Class UID", DicomUidType.Unknown);
-		public static string Version = "fo-dicom 1.0.13";
+		public static string Version = GetImplementationVersion();
+
+		private static string GetImplementationVersion() {
+			var version = Assembly.GetExecutingAssembly().GetName().Version;
+			return String.Format("fo-dicom {0}.{1}.{2}", version.Major, version.Minor, version.Build);
+		}
 	}
 }
