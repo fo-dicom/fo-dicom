7cf3fdf8d3019fbb30876043200e1cc3f0561974
Colby Dillion <colby.dillion@gmail.com>
2012-09-07 16:58:36 -0500

    pass codec parameters to input codec

--- DICOM/Imaging/Codec/DicomCodecExtensions.cs
+++ DICOM/Imaging/Codec/DicomCodecExtensions.cs
@@ -7,12 +7,14 @@ namespace Dicom.Imaging.Codec {
 	public static class DicomCodecExtensions {
 		public static DicomFile ChangeTransferSyntax(this DicomFile file, DicomTransferSyntax syntax, DicomCodecParams parameters = null) {
 			DicomTranscoder transcoder = new DicomTranscoder(file.FileMetaInfo.TransferSyntax, syntax);
+			transcoder.InputCodecParams = parameters;
 			transcoder.OutputCodecParams = parameters;
 			return transcoder.Transcode(file);
 		}
 
 		public static DicomDataset ChangeTransferSyntax(this DicomDataset dataset, DicomTransferSyntax syntax, DicomCodecParams parameters = null) {
 			DicomTranscoder transcoder = new DicomTranscoder(dataset.InternalTransferSyntax, syntax);
+			transcoder.InputCodecParams = parameters;
 			transcoder.OutputCodecParams = parameters;
 			return transcoder.Transcode(dataset);
 		}
