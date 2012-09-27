7770b29f36c835b311aa5eaa204f32c1fc7a5402
Colby Dillion <colby.dillion@gmail.com>
2012-09-26 22:36:08 -0500

    better handling for irregular specific character sets

--- ChangeLog.md
+++ ChangeLog.md
@@ -5,6 +5,7 @@
 * Fix encoding of JPEG2000 images with signed pixel data
 * Throttle queueing of PDUs to prevent out of memory errors for very large datasets
 * Better management of PDU buffer memory
+* Better handling for irregular specific character sets
 
 #### v1.0.22 (09/25/2012)
 * Add Offending Element tags to C-Move response output
--- DICOM/DicomEncoding.cs
+++ DICOM/DicomEncoding.cs
@@ -12,36 +12,37 @@ namespace Dicom {
 				return Default;
 
 			switch (charset.Trim()) {
-				case "ISO_IR 13":		return Encoding.GetEncoding(932);   // JIS X 0201 (Shift JIS) Unextended
-				case "ISO_IR 100":		return Encoding.GetEncoding(28591); // Latin Alphabet No. 1 Unextended
-				case "ISO_IR 101":		return Encoding.GetEncoding(28592); // Latin Alphabet No. 2 Unextended
-				case "ISO_IR 109":		return Encoding.GetEncoding(28593); // Latin Alphabet No. 3 Unextended
-				case "ISO_IR 110":		return Encoding.GetEncoding(28594); // Latin Alphabet No. 4 Unextended
-				case "ISO_IR 126":		return Encoding.GetEncoding(28597); // Greek Unextended
-				case "ISO_IR 127":		return Encoding.GetEncoding(28596); // Arabic Unextended
-				case "ISO_IR 138":		return Encoding.GetEncoding(28598); // Hebrew Unextended
-				case "ISO_IR 144":		return Encoding.GetEncoding(28595); // Cyrillic Unextended
-				case "ISO_IR 148":		return Encoding.GetEncoding(28599); // Latin Alphabet No. 5 (Turkish) Unextended
-				case "ISO_IR 166":		return Encoding.GetEncoding(874);   // TIS 620-2533 (Thai) Unextended
-				case "ISO_IR 192":		return Encoding.GetEncoding(65001); // Unicode in UTF-8
-				case "ISO 2022 IR 6":	return Encoding.GetEncoding(20127); // ASCII
-				case "ISO 2022 IR 13":	return Encoding.GetEncoding(50222); // JIS X 0201 (Shift JIS) Extended
-				case "ISO 2022 IR 87":	return Encoding.GetEncoding(50222); // JIS X 0208 (Kanji) Extended
-				case "ISO 2022 IR 100":	return Encoding.GetEncoding(28591); // Latin Alphabet No. 1 Extended
-				case "ISO 2022 IR 101":	return Encoding.GetEncoding(28592); // Latin Alphabet No. 2 Extended
-				case "ISO 2022 IR 109":	return Encoding.GetEncoding(28593); // Latin Alphabet No. 3 Extended
-				case "ISO 2022 IR 110":	return Encoding.GetEncoding(28594); // Latin Alphabet No. 4 Extended
-				case "ISO 2022 IR 127": return Encoding.GetEncoding(28596); // Arabic Extended
-				case "ISO 2022 IR 126": return Encoding.GetEncoding(28597); // Greek Extended
-				case "ISO 2022 IR 138": return Encoding.GetEncoding(28598); // Hebrew Extended
-				case "ISO 2022 IR 144":	return Encoding.GetEncoding(28595); // Cyrillic Extended
-				case "ISO 2022 IR 148":	return Encoding.GetEncoding(28599); // Latin Alphabet No. 5 (Turkish) Extended
-				case "ISO 2022 IR 149": return Encoding.GetEncoding(20949); // KS X 1001 (Hangul and Hanja) Extended
-				case "ISO 2022 IR 159": return Encoding.GetEncoding(50222); // JIS X 0212 (Kanji) Extended
-				case "ISO 2022 IR 166":	return Encoding.GetEncoding(874);   // TIS 620-2533 (Thai) Extended
-				case "GB18030":			return Encoding.GetEncoding(54936); // Chinese (Simplified) Extended
-				default:
-					throw new ArgumentException("No codepage found for requested DICOM charset.", "charset");
+			case "ISO IR 13":  case "ISO_IR 13":  return Encoding.GetEncoding(932);   // JIS X 0201 (Shift JIS) Unextended
+			case "ISO IR 100": case "ISO_IR 100": return Encoding.GetEncoding(28591); // Latin Alphabet No. 1 Unextended
+			case "ISO IR 101": case "ISO_IR 101": return Encoding.GetEncoding(28592); // Latin Alphabet No. 2 Unextended
+			case "ISO IR 109": case "ISO_IR 109": return Encoding.GetEncoding(28593); // Latin Alphabet No. 3 Unextended
+			case "ISO IR 110": case "ISO_IR 110": return Encoding.GetEncoding(28594); // Latin Alphabet No. 4 Unextended
+			case "ISO IR 126": case "ISO_IR 126": return Encoding.GetEncoding(28597); // Greek Unextended
+			case "ISO IR 127": case "ISO_IR 127": return Encoding.GetEncoding(28596); // Arabic Unextended
+			case "ISO IR 138": case "ISO_IR 138": return Encoding.GetEncoding(28598); // Hebrew Unextended
+			case "ISO IR 144": case "ISO_IR 144": return Encoding.GetEncoding(28595); // Cyrillic Unextended
+			case "ISO IR 148": case "ISO_IR 148": return Encoding.GetEncoding(28599); // Latin Alphabet No. 5 (Turkish) Unextended
+			case "ISO IR 166": case "ISO_IR 166": return Encoding.GetEncoding(874);   // TIS 620-2533 (Thai) Unextended
+			case "ISO IR 192": case "ISO_IR 192": return Encoding.GetEncoding(65001); // Unicode in UTF-8
+			case "ISO 2022 IR 6":	return Encoding.GetEncoding(20127); // ASCII
+			case "ISO 2022 IR 13":	return Encoding.GetEncoding(50222); // JIS X 0201 (Shift JIS) Extended
+			case "ISO 2022 IR 87":	return Encoding.GetEncoding(50222); // JIS X 0208 (Kanji) Extended
+			case "ISO 2022 IR 100":	return Encoding.GetEncoding(28591); // Latin Alphabet No. 1 Extended
+			case "ISO 2022 IR 101":	return Encoding.GetEncoding(28592); // Latin Alphabet No. 2 Extended
+			case "ISO 2022 IR 109":	return Encoding.GetEncoding(28593); // Latin Alphabet No. 3 Extended
+			case "ISO 2022 IR 110":	return Encoding.GetEncoding(28594); // Latin Alphabet No. 4 Extended
+			case "ISO 2022 IR 127": return Encoding.GetEncoding(28596); // Arabic Extended
+			case "ISO 2022 IR 126": return Encoding.GetEncoding(28597); // Greek Extended
+			case "ISO 2022 IR 138": return Encoding.GetEncoding(28598); // Hebrew Extended
+			case "ISO 2022 IR 144":	return Encoding.GetEncoding(28595); // Cyrillic Extended
+			case "ISO 2022 IR 148":	return Encoding.GetEncoding(28599); // Latin Alphabet No. 5 (Turkish) Extended
+			case "ISO 2022 IR 149": return Encoding.GetEncoding(20949); // KS X 1001 (Hangul and Hanja) Extended
+			case "ISO 2022 IR 159": return Encoding.GetEncoding(50222); // JIS X 0212 (Kanji) Extended
+			case "ISO 2022 IR 166":	return Encoding.GetEncoding(874);   // TIS 620-2533 (Thai) Extended
+			case "GB18030":			return Encoding.GetEncoding(54936); // Chinese (Simplified) Extended
+			default: // unknown encoding... return ASCII instead of throwing exception
+				//throw new ArgumentException("No codepage found for requested DICOM charset.", "charset");
+				return Encoding.ASCII;
 			}
 		}
 
