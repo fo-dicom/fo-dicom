b0a5978522912195469a2f09d10a36f107fb5788
Colby Dillion <colby.dillion@gmail.com>
2012-10-18 21:50:22 -0500

    remove unnecessary generic type from private classes

--- DICOM/Threading/ThreadPoolQueue.cs
+++ DICOM/Threading/ThreadPoolQueue.cs
@@ -4,17 +4,17 @@ using System.Threading;
 
 namespace Dicom.Threading {
 	class ThreadPoolQueue<T> {
-		private class WorkItem<T> {
+		private class WorkItem {
 			public T Group;
 			public Action Action;
 			public WaitCallback Callback;
 			public object State;
 		}
 
-		private class WorkGroup<T> {
+		private class WorkGroup {
 			public T Key;
 			public volatile bool Executing = false;
-			public Queue<WorkItem<T>> Items = new Queue<WorkItem<T>>();
+			public Queue<WorkItem> Items = new Queue<WorkItem>();
 
 			public WorkGroup(T key) {
 				Key = key;
@@ -22,39 +22,39 @@ namespace Dicom.Threading {
 		}
 
 		private object _lock = new object();
-		private Dictionary<T, WorkGroup<T>> _groups;
+		private Dictionary<T, WorkGroup> _groups;
 
 		public ThreadPoolQueue() {
-			_groups = new Dictionary<T, WorkGroup<T>>();
+			_groups = new Dictionary<T, WorkGroup>();
 		}
 
 		public void Queue(T group, Action action) {
-			Queue(new WorkItem<T> {
+			Queue(new WorkItem {
 				Group = group,
 				Action = action
 			});
 		}
 
 		public void Queue(T group, WaitCallback callback) {
-			Queue(new WorkItem<T> {
+			Queue(new WorkItem {
 				Group = group,
 				Callback = callback
 			});
 		}
 
 		public void Queue(T group, WaitCallback callback, object state) {
-			Queue(new WorkItem<T> {
+			Queue(new WorkItem {
 				Group = group,
 				Callback = callback,
 				State = state
 			});
 		}
 
-		private void Queue(WorkItem<T> item) {
+		private void Queue(WorkItem item) {
 			lock (_lock) {
-				WorkGroup<T> group = null;
+				WorkGroup group = null;
 				if (!_groups.TryGetValue(item.Group, out group)) {
-					group = new WorkGroup<T>(item.Group);
+					group = new WorkGroup(item.Group);
 					_groups.Add(item.Group, group);
 				}
 
@@ -66,7 +66,7 @@ namespace Dicom.Threading {
 
 		private void ExecuteNext(T groupKey) {
 			lock (_lock) {
-				WorkGroup<T> group = null;
+				WorkGroup group = null;
 				if (!_groups.TryGetValue(groupKey, out group))
 					return;
 
@@ -85,7 +85,7 @@ namespace Dicom.Threading {
 		}
 
 		private void ExecuteProc(object state) {
-			var item = (WorkItem<T>)state;
+			var item = (WorkItem)state;
 
 			try {
 				if (item.Action != null)
@@ -96,7 +96,7 @@ namespace Dicom.Threading {
 				// log this somewhere?
 			}
 
-			WorkGroup<T> group = null;
+			WorkGroup group = null;
 			if (!_groups.TryGetValue(item.Group, out group))
 				return;
 
