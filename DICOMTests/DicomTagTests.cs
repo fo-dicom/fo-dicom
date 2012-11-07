ba5bce1780722a2bd86f3f1115a7ee6012be78d9
Anders Gustafsson, Cureos AB <anders@cureos.com>
2012-11-07 22:19:05 +0100

    Addedu nit test project to prepare for DicomTag refactoring

---
+++ DICOMTests/DicomTagTests.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) 2012 Anders Gustafsson, Cureos AB.
+// Licensed under the Microsoft Public License (MS-PL).
+
+using Dicom.Helpers;
+using NUnit.Framework;
+
+namespace Dicom
+{
+    [TestFixture]
+    public class DicomTagTests
+    {
+        [Test]
+        public void Serialization_RegularTag_CanBeDeserialized()
+        {
+            var expected = DicomTag.BeamDose;
+            var actual = expected.GetSerializedDeserializedObject();
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void Serialization_PrivateCreatorTag_CanBeDeserialized()
+        {
+            var expected = new DicomTag(0x3005, 0x1013, "CUREOS");
+            var actual = expected.GetSerializedDeserializedObject();
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
