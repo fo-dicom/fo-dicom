488986f11957162d8c4e242567332dba6afe1c1d
Anders Gustafsson, Cureos AB <anders@cureos.com>
2012-11-07 22:31:09 +0100

    Added unit test for serialization of Point2

---
+++ DICOMTests/Imaging/Mathematics/Point2Tests.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) 2012 Anders Gustafsson, Cureos AB.
+// Licensed under the Microsoft Public License (MS-PL).
+
+using Dicom.Helpers;
+using NUnit.Framework;
+
+namespace Dicom.Imaging.Mathematics
+{
+    [TestFixture]
+    public class Point2Tests
+    {
+        [Test]
+        public void Serialization_RegularPoint_CanBeDeserialized()
+        {
+            var expected = new Point2(3, -5);
+            var actual = expected.GetSerializedDeserializedObject();
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
