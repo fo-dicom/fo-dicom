c2045c7b8a3ff4a3bdd5919dcab632f7b0e80c6e
Anders Gustafsson, Cureos AB <anders@cureos.com>
2013-01-07 16:13:04 +0100

    Added adapters to facilitate application on different platforms

---
+++ Portable.DICOM.Adapters/System/IO/File.cs
@@ -0,0 +1,34 @@
+﻿// ReSharper disable CheckNamespace
+namespace System.IO
+// ReSharper restore CheckNamespace
+{
+    public class File
+    {
+        #region METHODS
+
+        public static bool Exists(string path)
+        {
+            return true;
+        }
+
+        public static void Delete(string path)
+        {
+        }
+
+        public static Stream OpenRead(string path)
+        {
+            return Stream.Null;
+        }
+
+        public static byte[] ReadAllBytes(string path)
+        {
+            return new byte[0];
+        }
+
+        public static void WriteAllBytes(string path, byte[] bytes)
+        {
+        }
+
+        #endregion
+    }
+}
---
+++ Portable.DICOM.Adapters/System/Threading/Timer.cs
@@ -0,0 +1,27 @@
+﻿// ReSharper disable CheckNamespace
+namespace System.Threading
+// ReSharper restore CheckNamespace
+{
+    public delegate void TimerCallback(object state);
+
+    public class Timer
+    {
+        #region CONSTRUCTORS
+
+        public Timer(TimerCallback callback)
+        {
+            
+        }
+
+        #endregion
+
+        #region METHODS
+
+        public bool Change(int dueTime, int period)
+        {
+            return true;
+        }
+
+        #endregion
+    }
+}
