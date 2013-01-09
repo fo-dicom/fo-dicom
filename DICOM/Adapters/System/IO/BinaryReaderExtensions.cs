0540f6105c89fff1d1f777affbbd05f767e4ee45
Anders Gustafsson, Cureos AB <anders@cureos.com>
2013-01-09 12:48:40 +0100

    Added Close method for BinaryReader

---
+++ DICOM/Adapters/System/IO/BinaryReaderExtensions.cs
@@ -0,0 +1,12 @@
+﻿// ReSharper disable CheckNamespace
+namespace System.IO
+// ReSharper restore CheckNamespace
+{
+    public static class BinaryReaderExtensions
+    {
+         public static void Close(this BinaryReader reader)
+         {
+             reader.Dispose();
+         }
+    }
+}
