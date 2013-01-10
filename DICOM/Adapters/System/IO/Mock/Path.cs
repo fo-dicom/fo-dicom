2c1ef6c4aa896c9b806e496821fc480e8b4cfdcc
Anders Gustafsson, Cureos AB <anders@cureos.com>
2013-01-10 12:51:31 +0100

    Added mock classes to enable Windows Store compilation

---
+++ DICOM/Adapters/System/IO/Mock/Path.cs
@@ -0,0 +1,27 @@
+﻿// ReSharper disable CheckNamespace
+namespace System.IO.Mock
+// ReSharper restore CheckNamespace
+{
+    public static class Path
+    {
+        public static string GetTempPath()
+        {
+            return String.Empty;
+        }
+
+        public static string GetTempFileName()
+        {
+            return String.Empty;
+        }
+
+        public static string GetFullPath(string path)
+        {
+            return String.Empty;
+        }
+
+        public static string Combine(string path1, string path2)
+        {
+            return global::System.IO.Path.Combine(path1, path2);
+        }
+    }
+}
---
+++ DICOM/Adapters/System/Reflection/Mock/Assembly.cs
@@ -0,0 +1,12 @@
+﻿// ReSharper disable CheckNamespace
+namespace System.Reflection.Mock
+// ReSharper restore CheckNamespace
+{
+    public static class Assembly
+    {
+        public static global::System.Reflection.Assembly GetExecutingAssembly()
+        {
+            return typeof(Assembly).GetTypeInfo().Assembly;
+        }
+    }
+}
