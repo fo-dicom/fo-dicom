4e0319313c3107dbe1e0583286c878f24cf3f934
Anders Gustafsson, Cureos AB <anders@cureos.com>
2013-01-10 13:53:40 +0100

    Dummy adaptation code to enable Windows Store compilation

---
+++ DICOM/Adapters/System/ComponentModel/Composition/Hosting/CompositionContainer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+// ReSharper disable CheckNamespace
+namespace System.ComponentModel.Composition.Hosting
+// ReSharper restore CheckNamespace
+{
+    public class CompositionContainer
+    {
+        private DirectoryCatalog _catalog;
+
+        public CompositionContainer(DirectoryCatalog catalog)
+        {
+            // TODO: Complete member initialization
+            _catalog = catalog;
+        }
+
+        public IEnumerable<Lazy<T>> GetExports<T>()
+        {
+            return null;
+        }
+    }
+}
---
+++ DICOM/Adapters/System/Reflection/AssemblyExtensions.cs
@@ -0,0 +1,12 @@
+﻿// ReSharper disable CheckNamespace
+namespace System.Reflection
+// ReSharper restore CheckNamespace
+{
+    public static class AssemblyExtensions
+    {
+        public static string GetLocation(this Assembly assembly)
+        {
+            return String.Empty;
+        }
+    }
+}
