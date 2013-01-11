a46743e34047233fb9be7b7ba77195f8f0bdc402
Anders Gustafsson, Cureos AB <anders@cureos.com>
2013-01-11 12:14:30 +0100

    Implemented SortedList to return sorted items

--- DICOM/Adapters/System/Collections/Generic/SortedList.cs
+++ DICOM/Adapters/System/Collections/Generic/SortedList.cs
@@ -1,8 +1,27 @@
-﻿// ReSharper disable CheckNamespace
+﻿using System.Linq;
+
+// ReSharper disable CheckNamespace
 namespace System.Collections.Generic
 // ReSharper restore CheckNamespace
 {
-    public class SortedList<TKey, TValue> : Dictionary<TKey, TValue>
+    public class SortedList<TKey, TValue> : Dictionary<TKey, TValue>, IEnumerable<KeyValuePair<TKey, TValue>>
     {
+        public new IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator()
+        {
+            return this.OrderBy(kv => kv.Key).GetEnumerator();
+        }
+
+        IEnumerator<KeyValuePair<TKey, TValue>> IEnumerable<KeyValuePair<TKey, TValue>>.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public new ValueCollection Values
+        {
+            get
+            {
+                return this.OrderBy(kv => kv.Key).ToDictionary(kv => kv.Key, kv => kv.Value).Values;
+            }
+        }
     }
 }
