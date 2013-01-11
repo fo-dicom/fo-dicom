cd7281d9ef622a996a1ca226874adcc81a74a24e
Anders Gustafsson, Cureos AB <anders@cureos.com>
2013-01-11 08:34:54 +0100

    Added new files from main project into Store project
    Also re-entered ActionQueue and made the necessary updates of the
    "adapter" Thread and Directory classes

--- DICOM/Adapters/System/IO/Directory.cs
+++ DICOM/Adapters/System/IO/Directory.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System.Linq;
+using System.Threading.Tasks;
 using Windows.Storage;
 
 // ReSharper disable CheckNamespace
@@ -23,6 +24,28 @@ namespace System.IO
         public static void CreateDirectory(string path)
         {
             var folder = Task.Run(async () => await KnownFolders.DocumentsLibrary.CreateFolderAsync(path)).Result;
+        }
+
+        public static string[] GetDirectories(string path)
+        {
+            var folders = Task.Run(async () =>
+                                             {
+                                                 var root = await KnownFolders.DocumentsLibrary.GetFolderAsync(path);
+                                                 return await root.GetFoldersAsync();
+                                             }).Result;
+            return folders.Select(folder => folder.Name).ToArray();
+        }
+
+        public static string[] GetFiles(string path, string searchPattern = "*")
+        {
+            var files = Task.Run(async () =>
+                                           {
+                                               var root = await KnownFolders.DocumentsLibrary.GetFolderAsync(path);
+                                               return await root.GetFilesAsync();
+                                           }).Result;
+
+            // TODO Handle scenario with "real" search patterns
+            return files.Select(folder => folder.Name).ToArray();
         }
     }
 }
--- DICOM/Adapters/System/Threading/Thread.cs
+++ DICOM/Adapters/System/Threading/Thread.cs
@@ -1,11 +1,62 @@
-﻿// ReSharper disable CheckNamespace
+﻿using System.Threading.Tasks;
+
+// ReSharper disable CheckNamespace
 namespace System.Threading
 // ReSharper restore CheckNamespace
 {
-    public static class Thread
+    public class Thread
     {
-         public static void Sleep(int millisecondsTimeout)
-         {
-         }
+        #region FIELDS
+
+        private readonly Action _start;
+        private readonly Action<object> _parameterizedStart;
+        private Task _task;
+
+        #endregion
+
+        #region CONSTRUCTORS
+
+        public Thread(Action start)
+        {
+            _start = start;
+            _parameterizedStart = null;
+            _task = null;
+        }
+
+        public Thread(Action<object> parameterizedStart)
+        {
+            _start = null;
+            _parameterizedStart = parameterizedStart;
+            _task = null;
+        }
+
+        #endregion
+
+        #region PROPERTIES
+
+        public bool IsBackground { get; set; }
+
+        #endregion
+
+        #region METHODS
+
+        public void Start()
+        {
+            if (_start == null) throw new InvalidOperationException("Parameter-less action not defined for this thread instance.");
+            _task = Task.Run(_start);
+        }
+
+        public void Start(object parameter)
+        {
+            if (_parameterizedStart == null) throw new InvalidOperationException("Parameterized action not defined for this thread instance.");
+            _task = Task.Run(() => _parameterizedStart(parameter));
+        }
+
+        public static void Sleep(int millisecondsTimeout)
+        {
+            new ManualResetEvent(false).WaitOne(millisecondsTimeout);
+        }
+
+        #endregion
     }
 }
