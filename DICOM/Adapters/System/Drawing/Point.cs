6774a489cdbe87f33c7e7b50f0b9beb561323aaa
Anders Gustafsson, Cureos AB <anders@cureos.com>
2013-01-14 11:08:55 +0100

    Changed Point from class to struct, added unit tests

--- DICOM/Adapters/System/Drawing/Point.cs
+++ DICOM/Adapters/System/Drawing/Point.cs
@@ -2,11 +2,14 @@
 namespace System.Drawing
 // ReSharper restore CheckNamespace
 {
-    public class Point
+    public struct Point
     {
         #region FIELDS
 
         public static readonly Point Empty = new Point(0, 0);
+
+        private int _x;
+        private int _y;
 
         #endregion
 
@@ -14,17 +17,62 @@ namespace System.Drawing
 
         public Point(int x, int y)
         {
-            X = x;
-            Y = y;
+            _x = x;
+            _y = y;
         }
 
         #endregion
 
         #region PROPERTIES
 
-        public int X { get; set; }
+        public int X
+        {
+            get { return _x; }
+            set { _x = value; }
+        }
 
-        public int Y { get; set; }
+        public int Y
+        {
+            get { return _y; }
+            set { _y = value; }
+        }
+
+        #endregion
+
+        #region METHODS
+
+        public bool Equals(Point other)
+        {
+            return _x == other._x && _y == other._y;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            return obj is Point && Equals((Point)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return (_x * 397) ^ _y;
+            }
+        }
+
+        #endregion
+
+        #region OPERATORS
+
+        public static bool operator ==(Point lhs, Point rhs)
+        {
+            return rhs.Equals(lhs);
+        }
+
+        public static bool operator !=(Point lhs, Point rhs)
+        {
+            return !(lhs == rhs);
+        }
 
         #endregion
     }
---
+++ Store.DICOM [Unit Tests]/Adapters/System/Drawing/PointTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+
+// ReSharper disable CheckNamespace
+namespace System.Drawing
+// ReSharper restore CheckNamespace
+{
+    [TestClass]
+    public class PointTests
+    {
+        [TestMethod]
+        public void EqualityOperator_ComparingEqualObjects_YieldsTrue()
+        {
+            var pnt1 = new Point(3, 5);
+            var pnt2 = new Point { X = 3, Y = 5 };
+            Assert.IsTrue(pnt1 == pnt2);
+        }
+
+        [TestMethod]
+        public void EqualityOperator_ComparingNonEqualObjects_YieldsFalse()
+        {
+            var pnt1 = new Point(3, 4);
+            var pnt2 = new Point { X = 3, Y = 5 };
+            Assert.IsFalse(pnt1 == pnt2);
+        }
+    }
+}
