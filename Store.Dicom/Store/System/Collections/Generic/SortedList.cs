486b3a93b9b86fbc55bd21f265aa5b0d6b3a2dbb
Anders Gustafsson, Cureos AB <anders@cureos.com>
2013-02-06 22:57:00 +0100

    Let SortedList implementation inherit SortedDictionary

--- Store.Dicom/Store/System/Collections/Generic/SortedList.cs
+++ Store.Dicom/Store/System/Collections/Generic/SortedList.cs
@@ -1,27 +1,8 @@
-﻿using System.Linq;
-
-// ReSharper disable CheckNamespace
+﻿// ReSharper disable CheckNamespace
 namespace System.Collections.Generic
 // ReSharper restore CheckNamespace
 {
-    public class SortedList<TKey, TValue> : Dictionary<TKey, TValue>, IEnumerable<KeyValuePair<TKey, TValue>>
+    public class SortedList<TKey, TValue> : SortedDictionary<TKey, TValue>
     {
-        public new IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator()
-        {
-            return this.OrderBy(kv => kv.Key).GetEnumerator();
-        }
-
-        IEnumerator<KeyValuePair<TKey, TValue>> IEnumerable<KeyValuePair<TKey, TValue>>.GetEnumerator()
-        {
-            return GetEnumerator();
-        }
-
-        public new ValueCollection Values
-        {
-            get
-            {
-                return this.OrderBy(kv => kv.Key).ToDictionary(kv => kv.Key, kv => kv.Value).Values;
-            }
-        }
     }
 }
