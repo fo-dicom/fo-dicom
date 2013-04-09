02835d5efb5b47648657db8dcdca1b2a868d8e41
Anders Gustafsson, Cureos AB <anders@cureos.com>
2013-04-09 09:21:33 +0200

    Corrected GetExecutingAssembly method to work when invoked from other assembly.

--- Store.Net/Reflection/Assembly.cs
+++ Store.Net/Reflection/Assembly.cs
@@ -14,9 +14,21 @@ namespace Override.System.Reflection
 {
     public static class Assembly
     {
+		/// <summary>
+		/// Gets the assembly that contains the code that is currently executing.
+		/// </summary>
+		/// <returns>The assembly that contains the code that is currently executing.</returns>
+		/// <remarks>
+		/// By definition, the executing assembly is the assembly from which this method is invoked.
+		/// The return value should thus be the assembly from which this method is called.
+		/// The "classic" method used to obtain this assembly, <code>Assembly.GetCallingAssembly</code> is not
+		/// publicly exposed in the .NET for Windows Store assembly. Therefore the method is
+		/// here invoked through reflection, based on a StackOverflow tip at http://stackoverflow.com/a/14754653/650012 .
+		/// </remarks>
         public static global::System.Reflection.Assembly GetExecutingAssembly()
         {
-            return typeof(Assembly).GetTypeInfo().Assembly;
+			return (global::System.Reflection.Assembly)typeof(global::System.Reflection.Assembly).GetTypeInfo()
+				.GetDeclaredMethod("GetCallingAssembly").Invoke(null, new object[0]);
         }
     }
 }
