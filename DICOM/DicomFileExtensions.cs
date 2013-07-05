29058e155a406dd698a47c0c65fdf06555960ce9
Colby Dillion <colby.dillion@gmail.com>
2013-07-05 10:30:11 -0500

    extension method to clone DicomFile

---
+++ DICOM/DicomFileExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Dicom {
+	public static class DicomFileExtensions {
+		public static DicomFile Clone(this DicomFile original) {
+			var df = new DicomFile();
+			df.FileMetaInfo.Add(original.FileMetaInfo);
+			df.Dataset.Add(original.Dataset);
+			return df;
+		}
+	}
+}
