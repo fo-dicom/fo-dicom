25d37aa3eaa0e9281a078f37d6d116c3d66c5899
Colby Dillion <colby.dillion@gmail.com>
2013-07-13 17:37:29 -0500

    clip overlay to image region

--- DICOM/Imaging/Render/OverlayGraphic.cs
+++ DICOM/Imaging/Render/OverlayGraphic.cs
@@ -56,11 +56,11 @@ namespace Dicom.Imaging.Render {
 			int oy = (int)(_offsetY * _scale);
 
 			Parallel.For(0, _scaledData.Height, y => {
-				if (y > height)
+				if (y >= height)
 					return;
 				for (int i = _scaledData.Width * y, e = i + _scaledData.Width; i < e; i++) {
 					if (data[i] > 0) {
-						if (i > width)
+						if (i >= width)
 							break;
 						int p = (oy * width) + ox + i;
 						pixels[p] = _color;
