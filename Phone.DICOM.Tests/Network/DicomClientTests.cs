81139291f6ba442a0bc8e59013c562e72be8c147
Anders Gustafsson, Cureos AB <anders@cureos.com>
2013-08-29 09:42:59 +0200

    Added unit test project for phone libraries

---
+++ Phone.DICOM.Tests/Network/DicomClientTests.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+
+namespace Dicom.Network
+{
+	[TestClass]
+	public class DicomClientTests
+	{
+		[TestMethod]
+		public void Send_ToDicomServer_ReturnsSuccess()
+		{
+			var client = new DicomClient();
+			client.NegotiateAsyncOps();
+
+			DicomStatus status = null;
+			var request = new DicomCEchoRequest { OnResponseReceived = (echoRequest, response) => status = response.Status };
+			client.AddRequest(request);
+			client.Send("server", 104, false, "cureos", "cureos");
+
+			while (status == null) Thread.Sleep(10);
+			Assert.AreEqual(DicomStatus.Success, status);
+		}
+	}
+}
