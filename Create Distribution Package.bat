5e5f0ac3082ff1b69c163e8817e837581744b675
Colby Dillion <colby.dillion@gmail.com>
2014-02-19 16:38:21 -0600

    update NLog to 2.1.0

--- Create Distribution Package.bat
+++ Create Distribution Package.bat
@@ -9,17 +9,17 @@ copy "DICOM\bin\Release\Dicom.pdb" Distribute\Bin\ /Y
 copy "DICOM\bin\Release\Dicom.xml" Distribute\Bin\ /Y
 copy "DICOM [Native]\Release\Dicom.Native.dll" Distribute\Bin\ /Y
 copy "DICOM [Native]\Release\Dicom.Native64.dll" Distribute\Bin\ /Y
-copy "packages\NLog.2.0.0.2000\lib\net40\NLog.dll" Distribute\Bin\ /Y
+copy "packages\NLog.2.1.0\lib\net40\NLog.dll" Distribute\Bin\ /Y
 mkdir Distribute\Examples\
 copy "Examples\C-Store SCP\bin\Release\Dicom.CStoreSCP.exe" Distribute\Examples\ /Y
 copy "Examples\DICOM Media\bin\Release\Dicom.Media.exe" Distribute\Examples\ /Y
 copy "DICOM\bin\Release\Dicom.dll" Distribute\Examples\ /Y
 copy "DICOM [Native]\Release\Dicom.Native.dll" Distribute\Examples\ /Y
 copy "DICOM [Native]\Release\Dicom.Native64.dll" Distribute\Examples\ /Y
-copy "packages\NLog.2.0.0.2000\lib\net40\NLog.dll" Distribute\Examples\ /Y
+copy "packages\NLog.2.1.0\lib\net40\NLog.dll" Distribute\Examples\ /Y
 mkdir Distribute\Tools\
 copy "Tools\DICOM Dump\bin\Release\Dicom.Dump.exe" Distribute\Tools\ /Y
 copy "DICOM\bin\Release\Dicom.dll" Distribute\Tools\ /Y
 copy "DICOM [Native]\Release\Dicom.Native.dll" Distribute\Tools\ /Y
 copy "DICOM [Native]\Release\Dicom.Native64.dll" Distribute\Tools\ /Y
-copy "packages\NLog.2.0.0.2000\lib\net40\NLog.dll" Distribute\Tools\ /Y
+copy "packages\NLog.2.1.0\lib\net40\NLog.dll" Distribute\Tools\ /Y
