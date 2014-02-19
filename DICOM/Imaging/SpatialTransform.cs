0f6c342690c7d332ce6c9e7cbf087556c7a6f207
Anders Gustafsson <anders@cureos.com>
2014-02-19 11:53:27 +0100

    Removed dependency of System.Drawing.Point in Store/Phone

--- DICOM/Imaging/SpatialTransform.cs
+++ DICOM/Imaging/SpatialTransform.cs
@@ -1,5 +1,6 @@
-﻿using System;
+﻿#if !NETFX_CORE && !WINDOWS_PHONE && !TOUCH
 using System.Drawing;
+#endif
 
 namespace Dicom.Imaging {
 	public class SpatialTransform {
@@ -8,12 +9,19 @@ namespace Dicom.Imaging {
 		private int _rotate;
 		private bool _flipx;
 		private bool _flipy;
+#if NETFX_CORE || WINDOWS_PHONE || TOUCH
+	    private int _panX;
+	    private int _panY;
+#else
 		private Point _pan;
+#endif
 		#endregion
 
 		#region Public Constructors
 		public SpatialTransform() {
+#if !NETFX_CORE && !WINDOWS_PHONE && !TOUCH
 			_pan = new Point(0, 0);
+#endif
 			Reset();
 		}
 		#endregion
@@ -39,16 +47,34 @@ namespace Dicom.Imaging {
 			set { _flipy = value; }
 		}
 
+#if NETFX_CORE || WINDOWS_PHONE || TOUCH
+        public int PanX
+        {
+            get { return _panX; }
+            set { _panX = value; }
+        }
+
+        public int PanY
+        {
+            get { return _panY; }
+            set { _panY = value; }
+        }
+#else
 		public Point Pan {
 			get { return _pan; }
 			set { _pan = value; }
 		}
+#endif
 
 		public bool IsTransformed {
 			get {
 				return _scale != 1.0f ||
 					_rotate != 0.0f ||
-					_pan == Point.Empty;
+#if NETFX_CORE || WINDOWS_PHONE || TOUCH
+                    _panX != 0 || _panY != 0;
+#else
+					_pan != Point.Empty;
+#endif
 			}
 		}
 		#endregion
@@ -63,8 +89,13 @@ namespace Dicom.Imaging {
 			_rotate = 0;
 			_flipx = false;
 			_flipy = false;
+#if NETFX_CORE || WINDOWS_PHONE || TOUCH
+            _panX = 0;
+            _panY = 0;
+#else
 			_pan.X = 0;
 			_pan.Y = 0;
+#endif
 		}
 		#endregion
 	}
