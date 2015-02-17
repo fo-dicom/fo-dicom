f26efae70d0cd5b2dde89f17f8ae461238f7ac2b
zssure-thu <zssure@163.com>
2015-02-18 00:43:55 +0800

    Drawing DCM Demo
    This DrawingDCMExample.sln is a demo for retrieving the meta information
    of DCM file and painting the image to the screen.
    ****zssure****
    ****2015/02/18****

---
+++ Examples/DrawingDCM/DrawingDCMExample/DrawingDCMExample/Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace DrawingDCMExample
+{
+    partial class DICOMProcessDemo
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DICOMProcessDemo));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.closeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.Color.LightGray;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(823, 25);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.closeToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(39, 21);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // closeToolStripMenuItem
+            // 
+            this.closeToolStripMenuItem.Name = "closeToolStripMenuItem";
+            this.closeToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.closeToolStripMenuItem.Text = "Close";
+            this.closeToolStripMenuItem.Click += new System.EventHandler(this.closeToolStripMenuItem_Click);
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(47, 21);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(111, 22);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 25);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.listView1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.pictureBox1);
+            this.splitContainer1.Size = new System.Drawing.Size(823, 488);
+            this.splitContainer1.SplitterDistance = 275;
+            this.splitContainer1.TabIndex = 1;
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView1.GridLines = true;
+            this.listView1.Location = new System.Drawing.Point(0, 0);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(275, 488);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Tag = "Group Tag";
+            this.columnHeader1.Text = "GroupTag";
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Tag = "Element Tag";
+            this.columnHeader2.Text = "ElementTag";
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Tag = "Value";
+            this.columnHeader3.Text = "Value";
+            this.columnHeader3.Width = 155;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(544, 488);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // DICOMProcessDemo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(823, 513);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "DICOMProcessDemo";
+            this.Text = "DCMProcessDemo";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem closeToolStripMenuItem;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+    }
+}
+
---
+++ Examples/DrawingDCM/DrawingDCMExample/DrawingDCMExample/Form1.cs
@@ -0,0 +1,342 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+//using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Runtime;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters;
+using System.Runtime.Serialization.Formatters.Binary;
+using Dicom;
+using Dicom.IO;
+using Dicom.IO.Buffer;
+using Dicom.Imaging;
+using Dicom.Imaging.Codec;
+using Dicom.Imaging.Codec.Jpeg;
+using System.Runtime.InteropServices;
+
+
+namespace DrawingDCMExample
+{
+    public partial class DICOMProcessDemo : Form
+    {
+
+        /// <summary>
+        /// The Image member that inserted in PictureBox
+        /// </summary>
+        private string defaultImageBG;//the default background image
+        public DICOMProcessDemo()
+        {
+            InitializeComponent();
+            string appPath = Application.StartupPath;
+            string imagePath = appPath + @"\..\..\..\..\" + @"Resource\defaultbackgroundImage.bmp";
+            defaultImageBG = imagePath;
+            pictureBox1.Image = Image.FromFile(imagePath);
+            DicomTranscoder.LoadCodecs(Application.StartupPath, "Dicom.Native*.dll");
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            using(openFileDialog1 = new OpenFileDialog())
+            {
+                openFileDialog1.Filter="DCM files(*.DCM)|*.DCM| dcm files(*.dcm)|*.dcm";
+                openFileDialog1.RestoreDirectory=true;
+
+                if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    string dcmPath = openFileDialog1.FileName.ToString();
+                    //string dcmFileName = Path.Substring(Path.LastIndexOf("\\") + 1);
+                    listView1.Items.Clear();
+                    ShowAndDrawingDCMImage(dcmPath);
+
+                }
+            }
+        }
+        /// <summary>
+        /// Show the metaInfo of the DCM file and Drawing the image on the Screen
+        /// </summary>
+        /// <param name="dcmFileName">The path of your DCM file</param>
+        private void ShowAndDrawingDCMImage(string dcmFileName)
+        {
+            try
+            {
+                DicomFile dcmFile = DicomFile.Open(dcmFileName);
+                //Load and Show the meta info of the DCM file
+                LoadAndShowDCMMetaInfo(dcmFile);
+                listView1.Update();
+                //Drawing the image in your DCM file to screen.
+                DrawingDCMData2Screen(dcmFile);
+            }
+            catch (System.Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return;
+            }
+
+
+        }
+        /// <summary>
+        /// Load all of the Elements in the DCM Meta 
+        /// </summary>
+        /// <param name="dcm">the DICOM file</param>
+        private void LoadAndShowDCMMetaInfo(DicomFile dcm)
+        {
+            //Show the message of DCM file to the listview in the dialog.
+            DicomDataset dcmDataset = dcm.Dataset;
+            DicomFileMetaInformation dcmMetaInfo = dcm.FileMetaInfo;
+            listView1.BeginUpdate();
+            //DICOM MetaInfo
+            if (!String.IsNullOrWhiteSpace(dcmMetaInfo.MediaStorageSOPClassUID==null?"":dcmMetaInfo.MediaStorageSOPClassUID.ToString()))
+            {
+                //Insert the ImplementationClassUID
+                ListViewItem newItem = new ListViewItem("0002");
+                newItem.SubItems.Add("0002");
+                newItem.SubItems.Add(dcmMetaInfo.MediaStorageSOPClassUID.ToString());
+                listView1.Items.Add(newItem);
+            }
+            if (!String.IsNullOrWhiteSpace(dcmMetaInfo.MediaStorageSOPInstanceUID==null?"":dcmMetaInfo.MediaStorageSOPInstanceUID.ToString()))
+            {
+                //Insert the ImplementationClassUID
+                ListViewItem newItem = new ListViewItem("0002");
+                newItem.SubItems.Add("0003");
+                newItem.SubItems.Add(dcmMetaInfo.MediaStorageSOPInstanceUID.ToString());
+                listView1.Items.Add(newItem);
+
+            }
+            if (!String.IsNullOrWhiteSpace(dcmMetaInfo.TransferSyntax==null?"":dcmMetaInfo.TransferSyntax.ToString()))
+            {
+                //Insert the ImplementationClassUID
+                ListViewItem newItem = new ListViewItem("0002");
+                newItem.SubItems.Add("0010");
+                newItem.SubItems.Add(dcmMetaInfo.TransferSyntax.ToString());
+                listView1.Items.Add(newItem);
+
+            }
+            if (!String.IsNullOrWhiteSpace(dcmMetaInfo.ImplementationClassUID==null?"":dcmMetaInfo.ImplementationClassUID.ToString()))
+            {
+                //Insert the ImplementationClassUID
+                ListViewItem newItem = new ListViewItem("0002");
+                newItem.SubItems.Add("0012");
+                newItem.SubItems.Add(dcmMetaInfo.ImplementationClassUID.ToString());
+                listView1.Items.Add(newItem);
+
+            }
+            if (!String.IsNullOrWhiteSpace(dcmMetaInfo.ImplementationVersionName==null?"":dcmMetaInfo.ImplementationVersionName.ToString()))
+            {
+                //Insert the ImplementationClassUID
+                ListViewItem newItem = new ListViewItem("0002");
+                newItem.SubItems.Add("0013");
+                newItem.SubItems.Add(dcmMetaInfo.ImplementationVersionName.ToString());
+                listView1.Items.Add(newItem);
+
+            }
+
+            //Patient Info
+            string patientName = dcmDataset.Get<string>(DicomTag.PatientName, "");
+            if (!String.IsNullOrWhiteSpace(patientName))
+            {
+                ListViewItem newItem = new ListViewItem("0010");
+                newItem.SubItems.Add("0010");
+                newItem.SubItems.Add(patientName);
+                listView1.Items.Add(newItem);
+            }
+            string patientID = dcmDataset.Get<string>(DicomTag.PatientID, "");
+            if (!String.IsNullOrWhiteSpace(patientID))
+            {
+                ListViewItem newItem = new ListViewItem("0010");
+                newItem.SubItems.Add("0020");
+                newItem.SubItems.Add(patientID);
+                listView1.Items.Add(newItem);
+            }
+            string patientBirth = dcmDataset.Get<string>(DicomTag.PatientBirthDate);
+            if (!String.IsNullOrWhiteSpace(patientBirth))
+            {
+                ListViewItem newItem = new ListViewItem("0010");
+                newItem.SubItems.Add("0030");
+                newItem.SubItems.Add(patientBirth);
+                listView1.Items.Add(newItem);
+            }
+            string patientSex = dcmDataset.Get<string>(DicomTag.PatientSex);
+            if (!String.IsNullOrWhiteSpace(patientSex))
+            {
+                ListViewItem newItem = new ListViewItem("0010");
+                newItem.SubItems.Add("0040");
+                newItem.SubItems.Add(patientSex);
+                listView1.Items.Add(newItem);
+            }
+            string patientAge = dcmDataset.Get<string>(DicomTag.PatientAge);
+            if (!String.IsNullOrWhiteSpace(patientAge))
+            {
+                ListViewItem newItem = new ListViewItem("0010");
+                newItem.SubItems.Add("1010");
+                newItem.SubItems.Add(patientAge);
+                listView1.Items.Add(newItem);
+            }
+            //Study & Series Info
+            string studyDate = dcmDataset.Get<string>(DicomTag.StudyDate);
+            if (!String.IsNullOrWhiteSpace(studyDate))
+            {
+                ListViewItem newItem = new ListViewItem("0008");
+                newItem.SubItems.Add("0020");
+                newItem.SubItems.Add(studyDate);
+                listView1.Items.Add(newItem);
+            }
+            string studyTime = dcmDataset.Get<string>(DicomTag.StudyTime);
+            if (!String.IsNullOrWhiteSpace(studyTime))
+            {
+                ListViewItem newItem = new ListViewItem("0008");
+                newItem.SubItems.Add("0030");
+                newItem.SubItems.Add(studyTime);
+                listView1.Items.Add(newItem);
+            }
+            string studyInstanceID = dcmDataset.Get<string>(DicomTag.StudyInstanceUID);
+            if (!String.IsNullOrWhiteSpace(studyInstanceID))
+            {
+                ListViewItem newItem = new ListViewItem("0020");
+                newItem.SubItems.Add("000D");
+                newItem.SubItems.Add(studyInstanceID);
+                listView1.Items.Add(newItem);
+            }
+            string seriesInstanceID = dcmDataset.Get<string>(DicomTag.SeriesInstanceUID);
+            if (!String.IsNullOrWhiteSpace(seriesInstanceID))
+            {
+                ListViewItem newItem = new ListViewItem("0020");
+                newItem.SubItems.Add("000E");
+                newItem.SubItems.Add(seriesInstanceID);
+                listView1.Items.Add(newItem);
+            }
+            string studyID = dcmDataset.Get<string>(DicomTag.StudyID);
+            if (!String.IsNullOrWhiteSpace(studyID))
+            {
+                ListViewItem newItem = new ListViewItem("0020");
+                newItem.SubItems.Add("0010");
+                newItem.SubItems.Add(studyID);
+                listView1.Items.Add(newItem);
+            }
+            //Image Info
+            string imageHeight = dcmDataset.Get<string>(DicomTag.Rows);
+            if (!String.IsNullOrWhiteSpace(imageHeight))
+            {
+                ListViewItem newItem = new ListViewItem("0028");
+                newItem.SubItems.Add("0010");
+                newItem.SubItems.Add(imageHeight);
+                listView1.Items.Add(newItem);
+            }
+            string imageWidth = dcmDataset.Get<string>(DicomTag.Columns);
+            if (!String.IsNullOrWhiteSpace(imageWidth))
+            {
+                ListViewItem newItem = new ListViewItem("0028");
+                newItem.SubItems.Add("0011");
+                newItem.SubItems.Add(imageWidth);
+                listView1.Items.Add(newItem);
+            }
+            string bitsAlloctaed = dcmDataset.Get<string>(DicomTag.BitsAllocated);
+            if (!String.IsNullOrWhiteSpace(bitsAlloctaed))
+            {
+                ListViewItem newItem = new ListViewItem("0028");
+                newItem.SubItems.Add("0100");
+                newItem.SubItems.Add(bitsAlloctaed);
+                listView1.Items.Add(newItem);
+            }
+            string bitsStored = dcmDataset.Get<string>(DicomTag.BitsStored);
+            if (!String.IsNullOrWhiteSpace(bitsStored))
+            {
+                ListViewItem newItem = new ListViewItem("0028");
+                newItem.SubItems.Add("0101");
+                newItem.SubItems.Add(bitsStored);
+                listView1.Items.Add(newItem);
+            }
+            string highBits = dcmDataset.Get<string>(DicomTag.HighBit);
+            if (!String.IsNullOrWhiteSpace(highBits))
+            {
+                ListViewItem newItem = new ListViewItem("0028");
+                newItem.SubItems.Add("0102");
+                newItem.SubItems.Add(highBits);
+                listView1.Items.Add(newItem);
+            }
+            listView1.EndUpdate();
+
+        }
+        /// <summary>
+        /// Drawing the image in your DCM to the Dialog
+        /// </summary>
+        /// <param name="dcm"></param>
+        private void DrawingDCMData2Screen(DicomFile dcm)
+        {
+            pictureBox1.Image = ConvertDCMData2BitImage(dcm);
+        }
+
+        /// <summary>
+        /// Convert the byte data retrieved from DCM file to Image
+        /// </summary>
+        /// <param name="imagedata"></param>
+        /// <returns></returns>
+        private Bitmap ConvertDCMData2BitImage(DicomFile dcm)
+        {
+            DicomFile newDcmFile = null;
+            if (dcm.FileMetaInfo.TransferSyntax.IsEncapsulated)//if the data is compressed
+            {
+               // System.Reflection.Assembly.LoadFrom(Path.Combine(Application.StartupPath,"Dicom.Native64.dll"));
+                DicomTranscoder.LoadCodecs(Application.StartupPath, "Dicom.Native*.dll");
+                newDcmFile=dcm.ChangeTransferSyntax(DicomTransferSyntax.ExplicitVRLittleEndian,new DicomJpegLsParams());
+            }
+            DicomImage imageDcm=null;
+            if (newDcmFile != null)
+                imageDcm = new DicomImage(newDcmFile.Dataset);
+            else
+                imageDcm = new DicomImage(dcm.Dataset);
+            DicomDataset dataset = dcm.Dataset;
+            byte[] fs = imageDcm.PixelData.NumberOfFrames < 2 ? imageDcm.PixelData.GetFrame(0).Data : imageDcm.PixelData.GetFrame(1).Data;
+            uint size = (uint)Marshal.SizeOf(typeof(short));
+            uint padding = (uint)fs.Length % size;
+            uint count = (uint)fs.Length / size;
+            short[] values = new short[count];
+            System.Buffer.BlockCopy(fs, 0, values, 0, (int)(fs.Length - padding));
+
+            int height = dataset.Get<int>(DicomTag.Rows);
+            int width = dataset.Get<int>(DicomTag.Columns);
+            int windowsWidth = (int)dataset.Get<double>(DicomTag.WindowWidth);
+            int windowsCenter = (int)dataset.Get<double>(DicomTag.WindowCenter);
+            //if the windowsWidth = 0, the DCM file is not standard type.
+            if (windowsWidth == 0 || windowsCenter==0)
+            {
+                windowsWidth = values.Max<short>()-values.Min<short>();
+                windowsCenter = windowsWidth / 2;
+            }
+            int low = windowsCenter - windowsWidth / 2;
+            int high = windowsCenter + windowsWidth / 2;
+            Bitmap bitmap = new Bitmap(width, height);
+            for (int i = 0; i < height; ++i)
+            {
+                for (int j = 0; j < width; ++j)
+                {
+                    int r, g, b;
+                    int temp = (int)values[(width - j - 1) * height + i];
+                    int val = temp > high ? 255 : (temp < low ? 0 : ((temp - low) * 255 / windowsWidth));
+                    r = g = b = val;
+                    bitmap.SetPixel(i, width-j-1, Color.FromArgb(r, g, b));
+                }
+            }
+            return bitmap;
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("The program is just a demo written by C# using fo-dicom!****zssure****");
+        }
+
+        private void closeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            listView1.Items.Clear();
+            pictureBox1.Image = Image.FromFile(defaultImageBG);
+            MessageBox.Show("The close is just back to original state.Please click 'X' to exit! ");
+        }
+
+    }
+}
