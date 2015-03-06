58d2a73685559bd00ed094a6e9b701bf53c3fd1f
Rickard Holmberg <rickard.holmberg@raysearchlabs.com>
2015-03-06 22:43:32 +0100

    Move code generators to a new tool

---
+++ Tools/DicomJsonConverter/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DicomJsonConverter
+{
+	using System.IO;
+
+	using Dicom;
+	using Dicom.IO;
+
+	using Newtonsoft.Json;
+
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			if (args[0].EndsWith(".json"))
+			{
+				var json = File.ReadAllText(args[0]);
+				var dataset = JsonConvert.DeserializeObject<DicomDataset>(json, new JsonDicomConverter());
+				new DicomFile(dataset).Save(args[1]);
+			}
+			else if (args[0].EndsWith(".dcm"))
+			{
+				var ds = DicomFile.Open(args[0]).Dataset;
+				var json = JsonConvert.SerializeObject(ds, new JsonDicomConverter(true));
+				File.WriteAllText(args[1], json);
+			}
+		}
+	}
+}
