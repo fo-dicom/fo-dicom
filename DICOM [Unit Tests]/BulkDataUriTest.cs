849733e6d279220bd8dc7fd300531f760a21ec58
Rickard Holmberg <rickard.holmberg@raysearchlabs.com>
2015-03-17 00:22:52 +0100

    Require explicit downloading of bulk uri byte buffers

--- DICOM [Unit Tests]/BulkDataUriTest.cs
+++ DICOM [Unit Tests]/BulkDataUriTest.cs
@@ -13,18 +13,23 @@ using System.Text;
 
 namespace DICOM__Unit_Tests_
 {
-    public class BulkDataUriTest
-    {
-        [Fact]
-        public void TestReadBulkData()
-        {
-            var path = System.IO.Path.GetFullPath("test.txt");
-            var bulkData = new BulkUriByteBuffer("file:" + path);
+	public class BulkDataUriTest
+	{
+		[Fact]
+		public void TestReadBulkData()
+		{
+			var path = Path.GetFullPath("test.txt");
+			var bulkData = new BulkUriByteBuffer("file:" + path);
 
-            byte[] expected = File.ReadAllBytes("test.txt");
+			Assert.Throws<InvalidOperationException>(() => bulkData.Data);
+			Assert.Throws<InvalidOperationException>(() => bulkData.Size);
 
-            Assert.True(bulkData.Data.SequenceEqual(expected));
-            Assert.Equal(bulkData.Size, (uint)expected.Length);
-        }
-    }
+			bulkData.GetData();
+
+			byte[] expected = File.ReadAllBytes("test.txt");
+
+			Assert.True(bulkData.Data.SequenceEqual(expected));
+			Assert.Equal(bulkData.Size, (uint)expected.Length);
+		}
+	}
 }
--- DICOM/IO/Buffer/BulkUriByteBuffer.cs
+++ DICOM/IO/Buffer/BulkUriByteBuffer.cs
@@ -5,6 +5,9 @@ namespace Dicom.IO.Buffer
 {
 	public sealed class BulkUriByteBuffer : IByteBuffer
 	{
+		private byte[] _buffer;
+		private uint? _size;
+
 		public BulkUriByteBuffer(string bulkDataUri)
 		{
 			this.BulkDataUri = bulkDataUri;
@@ -14,7 +17,7 @@ namespace Dicom.IO.Buffer
 		{
 			get
 			{
-				return false;
+				return _buffer != null;
 			}
 		}
 
@@ -24,35 +27,76 @@ namespace Dicom.IO.Buffer
 		{
 			get
 			{
-				using (var response = WebRequest.Create(BulkDataUri).GetResponse())
-				{
-					int count = (int)response.ContentLength;
-					var buffer = new Byte[count];
-					response.GetResponseStream().Read(buffer, 0, count);
-					return buffer;
-				}
+				if (_buffer == null)
+					throw new InvalidOperationException(
+						"BulkUriByteBuffer cannot provide Data until either GetData() has been called.");
+				return _buffer;
 			}
+		}
+
+		public byte[] GetData()
+		{
+			if (_buffer != null) return _buffer;
+
+			using (var response = WebRequest.Create(BulkDataUri).GetResponse())
+			{
+				int count = (int)response.ContentLength;
+				var buffer = new Byte[count];
+				response.GetResponseStream().Read(buffer, 0, count);
+				_buffer = buffer;
+				_size = (uint)_buffer.LongLength;
+			}
+
+			return _buffer;
 		}
 
 		public byte[] GetByteRange(int offset, int count)
 		{
-			using (var response = WebRequest.Create(BulkDataUri).GetResponse())
+			if (_buffer == null)
 			{
-				var buffer = new Byte[count];
-				response.GetResponseStream().Read(buffer, offset, count);
-				return buffer;
+				GetData();
 			}
+
+			var range = new byte[count];
+			Array.Copy(Data, offset, range, 0, count);
+			return range;
 		}
 
 		public uint Size
 		{
 			get
 			{
-				using (var response = WebRequest.Create(BulkDataUri).GetResponse())
+				if (!_size.HasValue)
+					throw new InvalidOperationException(
+						"BulkUriByteBuffer cannot provide Size until either GetData() or GetSize() has been called.");
+
+				return _size.Value;
+			}
+		}
+
+		public uint GetSize()
+		{
+			if (_size.HasValue) return _size.Value;
+			try
+			{
+				var request = WebRequest.Create(BulkDataUri);
+				request.Method = "HEAD";
+				using (var response = request.GetResponse())
 				{
-					return (uint)response.ContentLength;
+					if (response.ContentLength != -1) return (uint)response.ContentLength;
 				}
 			}
+			catch (WebException)
+			{
+			}
+			catch (NotSupportedException)
+			{
+			}
+			catch (InvalidOperationException)
+			{
+			}
+			GetData();
+			return Size;
 		}
 	}
 }
