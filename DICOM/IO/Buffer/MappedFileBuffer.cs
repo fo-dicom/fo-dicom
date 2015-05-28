d707cd1d02ff36c66e5a12f9b072ab15f30f3f74
Hesham Desouky <hdesouky@gmail.com>
2015-05-28 14:07:03 +0200

    Added Mapped File Buffer DicomImage to not load image by default in constructor PixelData to use memory mapped file buffer instead of heap memory buffer to store pixel data

---
+++ DICOM/IO/Buffer/MappedFileBuffer.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.MemoryMappedFiles;
+using System.Linq;
+using System.Text;
+
+namespace Dicom.IO.Buffer
+{
+    public sealed class MappedFileBuffer : IByteBuffer, IDisposable
+    {
+        private MemoryMappedFile _file;
+        private MemoryMappedViewStream _stream;
+		private uint _size;
+        private bool _disposed = false;
+        public MappedFileBuffer(byte[] data)
+        {
+            _file = MemoryMappedFile.CreateNew(Guid.NewGuid().ToString(), data.Length);
+            _stream = _file.CreateViewStream();
+            _stream.Write(data, 0, data.Length);
+            
+			//File.WriteAllBytes(_file.Name, data);
+			_size = (uint)data.Length;
+		}
+
+        ~MappedFileBuffer()
+        {
+            Dispose();
+
+        }
+
+		public bool IsMemory {
+			get { return false; }
+		}
+
+		public uint Size {
+			get { return _size; }
+		}
+
+		public byte[] Data {
+			get 
+            {
+                return GetByteRange(0, (int)_size);
+            }
+		}
+
+		public byte[] GetByteRange(int offset, int count) {
+			byte[] buffer = new byte[count];
+
+            _stream.Seek(offset, SeekOrigin.Begin);
+            _stream.Read(buffer, 0, count);
+
+			return buffer;
+		}
+
+        #region IDisposable Members
+
+        public void Dispose()
+        {
+            if (!_disposed)
+            {
+                _stream.Close();
+                _stream.Dispose();
+                _file.Dispose();
+                _disposed = true;
+            }
+        }
+
+        #endregion
+    }
+}
