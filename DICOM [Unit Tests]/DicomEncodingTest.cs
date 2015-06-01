40545327cc534770cb52b3c83853394c37642f02
Anders Gustafsson <anders@cureos.com>
2015-06-01 15:16:32 +0200

    Added unit tests to check return correctness. Issue #21

---
+++ DICOM [Unit Tests]/DicomEncodingTest.cs
@@ -0,0 +1,26 @@
+﻿namespace Dicom
+{
+    using System.Text;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class DicomEncodingTest
+    {
+        [TestMethod]
+        public void Default_Getter_ReturnsUSASCII()
+        {
+            var expected = Encoding.ASCII.CodePage;
+            var actual = DicomEncoding.Default.CodePage;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void GetEncoding_NonMatchingCharset_ReturnsUSASCII()
+        {
+            var expected = Encoding.ASCII.CodePage;
+            var actual = DicomEncoding.GetEncoding("GBK").CodePage;
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
