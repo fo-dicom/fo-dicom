01a7151a81ee4e225d819ac1f2af57bffbae44b3
Anders Gustafsson <anders@cureos.com>
2015-08-19 17:51:39 +0200

    Separated memory mapped file opener to partial class file.

--- DICOM/DicomFile.cs
+++ DICOM/DicomFile.cs
@@ -11,7 +11,7 @@ using Dicom.IO.Writer;
 
 namespace Dicom
 {
-    public class DicomFile
+    public partial class DicomFile
     {
         public DicomFile()
         {
@@ -135,57 +135,6 @@ namespace Dicom
             result.AsyncWaitHandle.WaitOne();
 
             if (eventResult.InternalState != null) throw eventResult.InternalState as Exception;
-        }
-
-        /// <summary>
-        /// Reads a DICOM file with the specified filename, using default DICOM encoding, and returns a DicomFile object.
-        /// </summary>
-        /// <param name="fileName">The filename of the DICOM file.</param>
-        /// <returns>DicomFile instance.</returns>
-        /// <remarks>Uses MemoryMappedFile to avoid unneccesary copying of data into memory.</remarks>
-        public static DicomFile OpenLarge(string fileName)
-        {
-            return OpenLarge(fileName, DicomEncoding.Default);
-        }
-
-        /// <summary>
-        /// Reads a DICOM file with the specified filename and returns a DicomFile object.
-        /// </summary>
-        /// <param name="fileName">The filename of the DICOM file.</param>
-        /// <param name="fallbackEncoding">Encoding to apply when attribute Specific Character Set is not available.</param>
-        /// <returns>DicomFile instance.</returns>
-        /// <remarks>Uses MemoryMappedFile to avoid unneccesary copying of data into memory.</remarks>
-        public static DicomFile OpenLarge(string fileName, Encoding fallbackEncoding)
-        {
-            if (fallbackEncoding == null)
-            {
-                throw new ArgumentNullException("fallbackEncoding");
-            }
-            DicomFile df = new DicomFile();
-
-            try
-            {
-                df.File = new FileReference(fileName);
-
-                using (var source = new MemoryMappedFileByteSource(fileName))
-                {
-                    DicomFileReader reader = new DicomFileReader();
-                    reader.Read(
-                        source,
-                        new DicomDatasetReaderObserver(df.FileMetaInfo),
-                        new DicomDatasetReaderObserver(df.Dataset, fallbackEncoding));
-
-                    df.Format = reader.FileFormat;
-
-                    df.Dataset.InternalTransferSyntax = reader.Syntax;
-
-                    return df;
-                }
-            }
-            catch (Exception e)
-            {
-                throw new DicomFileException(df, e.Message, e);
-            }
         }
 
         /// <summary>
