c890e90cc5cee7f0a577a037f1eb1cf1543d7daf
Anders Gustafsson <anders@cureos.com>
2015-09-10 16:06:40 +0200

    Added unit test to expose Pan property. Issue #83

---
+++ DICOM [Unit Tests]/Imaging/SpatialTransformTest.cs
@@ -0,0 +1,30 @@
+﻿// Copyright (c) 2012-2015 fo-dicom contributors.
+// Licensed under the Microsoft Public License (MS-PL).
+
+namespace Dicom.Imaging
+{
+    using System.Drawing;
+
+    using Xunit;
+
+    public class SpatialTransformTest
+    {
+        #region Unit tests
+
+        [Fact]
+        public void IsTransformed_PanNonZero_IsTrue()
+        {
+            var transform = new SpatialTransform { Pan = new Point(5, -7) };
+            Assert.True(transform.IsTransformed);
+        }
+
+        [Fact]
+        public void IsTransformed_ScaleUnity_RotateZeroPanZero_IsFalse()
+        {
+            var transform = new SpatialTransform { Scale = 1.0, Rotation = 0, Pan = new Point(0, 0) };
+            Assert.False(transform.IsTransformed);
+        }
+
+        #endregion
+    }
+}
