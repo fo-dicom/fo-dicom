7aa527a71d5a7ff166daa12994d258c6d8964d5c
Anders Gustafsson <anders@cureos.com>
2015-10-09 14:34:42 +0200

    Added managers setup method. Issue #60

---
+++ DICOM/Managers.cs
@@ -0,0 +1,39 @@
+﻿// Copyright (c) 2012-2015 fo-dicom contributors.
+// Licensed under the Microsoft Public License (MS-PL).
+
+namespace Dicom
+{
+    using Dicom.Imaging;
+    using Dicom.Imaging.Codec;
+    using Dicom.IO;
+    using Dicom.Log;
+    using Dicom.Network;
+
+    /// <summary>
+    /// Helper class for initializing manager implementations.
+    /// </summary>
+    public static class Managers
+    {
+        /// <summary>
+        /// Setup implementations for all managers.
+        /// </summary>
+        /// <param name="ioManagerImpl">I/O manage implementation.</param>
+        /// <param name="networkManagerImpl">Network manager implementation.</param>
+        /// <param name="transcoderManagerImpl">DICOM transcoder manager implementation.</param>
+        /// <param name="imageManagerImpl">Image manager implementation.</param>
+        /// <param name="logManagerImpl">Log manager implementation.</param>
+        public static void Setup(
+            IOManager ioManagerImpl,
+            NetworkManager networkManagerImpl,
+            DicomTranscoderManager transcoderManagerImpl,
+            ImageManager imageManagerImpl,
+            LogManager logManagerImpl)
+        {
+            IOManager.SetImplementation(ioManagerImpl);
+            NetworkManager.SetImplementation(networkManagerImpl);
+            DicomTranscoderManager.SetImplementation(transcoderManagerImpl);
+            ImageManager.SetImplementation(imageManagerImpl);
+            LogManager.SetImplementation(logManagerImpl);
+        }
+    }
+}
