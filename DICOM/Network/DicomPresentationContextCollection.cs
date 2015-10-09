e9c74b6c8040a9920241b9e2a993694e3d7b5f98
Anders Gustafsson <anders@cureos.com>
2015-10-09 16:16:35 +0200

    Replaced SortedList with portable SortedDictionary. Issue #60

--- DICOM/Network/DicomPresentationContextCollection.cs
+++ DICOM/Network/DicomPresentationContextCollection.cs
@@ -9,11 +9,11 @@ namespace Dicom.Network
 {
     public class DicomPresentationContextCollection : ICollection<DicomPresentationContext>
     {
-        private SortedList<byte, DicomPresentationContext> _pc;
+        private SortedDictionary<byte, DicomPresentationContext> _pc;
 
         public DicomPresentationContextCollection()
         {
-            _pc = new SortedList<byte, DicomPresentationContext>();
+            _pc = new SortedDictionary<byte, DicomPresentationContext>();
         }
 
         public DicomPresentationContext this[byte id]
