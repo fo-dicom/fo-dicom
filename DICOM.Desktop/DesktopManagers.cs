9f2dfe884741ed20a23fc985762506d5dd04f1de
Anders Gustafsson <anders@cureos.com>
2015-10-09 15:22:05 +0200

    Added Dicom.Desktop project. Issue #60

---
+++ DICOM.Desktop/DesktopManagers.cs
@@ -0,0 +1,24 @@
+﻿// Copyright (c) 2012-2015 fo-dicom contributors.
+// Licensed under the Microsoft Public License (MS-PL).
+
+namespace Dicom
+{
+    using Dicom.Imaging;
+    using Dicom.Imaging.Codec;
+    using Dicom.IO;
+    using Dicom.Log;
+    using Dicom.Network;
+
+    public static class DesktopManagers
+    {
+        public static void Setup(ImageManager imageManagerImpl, LogManager logManagerImpl = null)
+        {
+            Managers.Setup(
+                DesktopIOManager.Instance,
+                DesktopNetworkManager.Instance,
+                DesktopTranscoderManager.Instance,
+                imageManagerImpl,
+                logManagerImpl);
+        }
+    }
+}
