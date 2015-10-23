5bdbb0335966375a245f2c4d4ec648fe7c79773f
Anders Gustafsson <anders@cureos.com>
2015-10-23 09:01:02 +0200

    Updated .nuspec files and build script. Issue #119

--- Setup/1_Build.cmd
+++ Setup/1_Build.cmd
@@ -4,14 +4,14 @@ echo.
 echo fo-dicom builder
 echo ================
 echo. 
-echo This Windows batch file will use Visual Studio 2013 to
+echo This Windows batch file will use Visual Studio 2015 to
 echo compile the Release versions of fo-dicom.
 echo. 
 
 timeout /T 5
 
-@if "%VS120COMNTOOLS%"=="" goto error_no_VS120COMNTOOLSDIR
-@call "%VS120COMNTOOLS%VsDevCmd.bat"
+@if "%VS140COMNTOOLS%"=="" goto error_no_VS140COMNTOOLSDIR
+@call "%VS140COMNTOOLS%VsDevCmd.bat"
 
 @cd ..
 @msbuild "DICOM.Full.sln" /t:Rebuild /p:Configuration=Release;Platform="Any CPU"
@@ -19,7 +19,7 @@ timeout /T 5
 @goto end
 
 @REM -----------------------------------------------------------------------
-:error_no_VS120COMNTOOLSDIR
+:error_no_VS140COMNTOOLSDIR
 @echo ERROR: Cannot determine the location of the VS Common Tools folder.
 @goto end
 
