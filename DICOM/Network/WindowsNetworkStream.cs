c8f7fecb6384e6adb2112bcba9b55b265b170e25
Anders Gustafsson <anders@cureos.com>
2015-11-02 10:05:50 +0100

    Renewed attempts to enable reading and writing network streams. Issue #118

--- DICOM/Network/WindowsNetworkStream.cs
+++ DICOM/Network/WindowsNetworkStream.cs
@@ -6,15 +6,17 @@ namespace Dicom.Network
     using System;
     using System.Globalization;
     using System.IO;
-    using System.Threading;
+    using System.Threading.Tasks;
 
     using Windows.Networking;
     using Windows.Networking.Sockets;
+    using Windows.Security.Cryptography.Certificates;
+    using Windows.Storage.Streams;
 
     /// <summary>
     /// Universal Windows Platform implementation of <see cref="INetworkStream"/>.
     /// </summary>
-    public sealed class WindowsNetworkStream : INetworkStream
+    public sealed class WindowsNetworkStream : Stream, INetworkStream
     {
         #region FIELDS
 
@@ -22,9 +24,7 @@ namespace Dicom.Network
 
         private readonly StreamSocket socket;
 
-        private Stream networkStream;
-
-        private readonly ManualResetEventSlim handle;
+        private readonly bool canDisposeSocket;
 
         #endregion
 
@@ -41,11 +41,18 @@ namespace Dicom.Network
         internal WindowsNetworkStream(string host, int port, bool useTls, bool noDelay, bool ignoreSslPolicyErrors)
         {
             this.socket = new StreamSocket();
-            this.socket.Control.NoDelay = noDelay;
-            // TODO Update socket.Control.IgnorableServerCertificateErrors with all possible errors if ignoreSslPolicyErrors is true?
+            this.canDisposeSocket = true;
 
-            this.handle = new ManualResetEventSlim(false);
-            this.InitializeNetworkStream(host, port, useTls);
+            this.socket.Control.NoDelay = noDelay;
+            if (ignoreSslPolicyErrors)
+            {
+                foreach (var value in Enum.GetValues(typeof(ChainValidationResult)))
+                {
+                    this.socket.Control.IgnorableServerCertificateErrors.Add((ChainValidationResult)value);
+                }
+            }
+
+            this.InitializeNetworkStream(host, port, useTls).Wait();
         }
 
         /// <summary>
@@ -57,10 +64,8 @@ namespace Dicom.Network
         /// is initialized with this server-side constructor.</remarks>
         internal WindowsNetworkStream(StreamSocket socket)
         {
-            this.networkStream = socket.InputStream.AsStreamForRead();
-            this.socket = null;
-
-            this.handle = new ManualResetEventSlim(true);
+            this.socket = socket;
+            this.canDisposeSocket = false;
         }
 
         /// <summary>
@@ -81,17 +86,7 @@ namespace Dicom.Network
         /// <returns>Network stream as <see cref="Stream"/> object.</returns>
         public Stream AsStream()
         {
-            this.handle.Wait();
-            return this.networkStream;
-        }
-
-        /// <summary>
-        /// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
-        /// </summary>
-        public void Dispose()
-        {
-            this.Dispose(true);
-            GC.SuppressFinalize(this);
+            return this;
         }
 
         /// <summary>
@@ -100,11 +95,11 @@ namespace Dicom.Network
         /// <param name="disposing">True if called from <see cref="Dispose"/>, false otherwise.</param>
         /// <remarks>The underlying stream is normally passed on to a <see cref="DicomService"/> implementation that
         /// is responsible for disposing the stream when appropriate. Therefore, the stream should not be disposed here.</remarks>
-        private void Dispose(bool disposing)
+        protected override void Dispose(bool disposing)
         {
             if (this.disposed) return;
 
-            if (this.socket != null)
+            if (this.canDisposeSocket && this.socket != null)
             {
                 this.socket.Dispose();
             }
@@ -112,23 +107,214 @@ namespace Dicom.Network
             this.disposed = true;
         }
 
-        private async void InitializeNetworkStream(string host, int port, bool useTls)
+        /// <summary>
+        /// When overridden in a derived class, clears all buffers for this stream and causes any buffered data to be written to the underlying device.
+        /// </summary>
+        /// <exception cref="T:System.IO.IOException">An I/O error occurs. </exception>
+        /// <filterpriority>2</filterpriority>
+        public override async void Flush()
         {
-            await
-                this.socket.ConnectAsync(new HostName(host), port.ToString(CultureInfo.InvariantCulture))
-                    .AsTask()
-                    .ConfigureAwait(false);
+            await this.socket.OutputStream.FlushAsync();
+        }
+
+        /// <summary>
+        /// When overridden in a derived class, reads a sequence of bytes from the current stream and advances the position within the stream by the number of bytes read.
+        /// </summary>
+        /// <returns>
+        /// The total number of bytes read into the buffer. This can be less than the number of bytes requested if that many bytes are not currently available, or zero (0) if the end of the stream has been reached.
+        /// </returns>
+        /// <param name="buffer">An array of bytes. When this method returns, the buffer contains the specified byte array with the values between <paramref name="offset"/> and (<paramref name="offset"/> + <paramref name="count"/> - 1) replaced by the bytes read from the current source. </param>
+        /// <param name="offset">The zero-based byte offset in <paramref name="buffer"/> at which to begin storing the data read from the current stream. </param>
+        /// <param name="count">The maximum number of bytes to be read from the current stream. </param>
+        /// <exception cref="T:System.ArgumentException">The sum of <paramref name="offset"/> and <paramref name="count"/> is larger than the buffer length. </exception>
+        /// <exception cref="T:System.ArgumentNullException"><paramref name="buffer"/> is null. </exception><exception cref="T:System.ArgumentOutOfRangeException"><paramref name="offset"/> or <paramref name="count"/> is negative. </exception>
+        /// <exception cref="T:System.IO.IOException">An I/O error occurs. </exception><exception cref="T:System.NotSupportedException">The stream does not support reading. </exception>
+        /// <exception cref="T:System.ObjectDisposedException">Methods were called after the stream was closed. </exception>
+        /// <filterpriority>1</filterpriority>
+        public override int Read(byte[] buffer, int offset, int count)
+        {
+            return DoReadAsync(this.socket.InputStream, buffer, offset, count).Result;
+        }
+
+        /// <summary>
+        /// When overridden in a derived class, sets the position within the current stream.
+        /// </summary>
+        /// <returns>
+        /// The new position within the current stream.
+        /// </returns>
+        /// <param name="offset">A byte offset relative to the <paramref name="origin"/> parameter. 
+        /// </param><param name="origin">A value of type <see cref="T:System.IO.SeekOrigin"/> indicating the reference point used to obtain the new position. </param>
+        /// <exception cref="T:System.IO.IOException">An I/O error occurs. </exception><exception cref="T:System.NotSupportedException">The stream does not support seeking, such as if the stream is constructed from a pipe or console output. </exception>
+        /// <exception cref="T:System.ObjectDisposedException">Methods were called after the stream was closed. </exception>
+        /// <filterpriority>1</filterpriority>
+        public override long Seek(long offset, SeekOrigin origin)
+        {
+            throw new NotSupportedException();
+        }
+
+        /// <summary>
+        /// When overridden in a derived class, sets the length of the current stream.
+        /// </summary>
+        /// <param name="value">The desired length of the current stream in bytes. </param>
+        /// <exception cref="T:System.IO.IOException">An I/O error occurs. </exception>
+        /// <exception cref="T:System.NotSupportedException">The stream does not support both writing and seeking, such as if the stream is constructed from a pipe or console output. </exception>
+        /// <exception cref="T:System.ObjectDisposedException">Methods were called after the stream was closed. </exception>
+        /// <filterpriority>2</filterpriority>
+        public override void SetLength(long value)
+        {
+            throw new NotSupportedException();
+        }
+
+        /// <summary>
+        /// When overridden in a derived class, writes a sequence of bytes to the current stream and advances the current position within this stream by the number of bytes written.
+        /// </summary>
+        /// <param name="buffer">An array of bytes. This method copies <paramref name="count"/> bytes from <paramref name="buffer"/> to the current stream. </param>
+        /// <param name="offset">The zero-based byte offset in <paramref name="buffer"/> at which to begin copying bytes to the current stream. </param>
+        /// <param name="count">The number of bytes to be written to the current stream. </param>
+        /// <exception cref="T:System.ArgumentException">The sum of <paramref name="offset"/> and <paramref name="count"/> is greater than the buffer length.</exception>
+        /// <exception cref="T:System.ArgumentNullException"><paramref name="buffer"/>  is null.</exception>
+        /// <exception cref="T:System.ArgumentOutOfRangeException"><paramref name="offset"/> or <paramref name="count"/> is negative.</exception>
+        /// <exception cref="T:System.IO.IOException">An I/O error occured, such as the specified file cannot be found.</exception>
+        /// <exception cref="T:System.NotSupportedException">The stream does not support writing.</exception>
+        /// <exception cref="T:System.ObjectDisposedException"><see cref="M:System.IO.Stream.Write(System.Byte[],System.Int32,System.Int32)"/> was called after the stream was closed.</exception>
+        /// <filterpriority>1</filterpriority>
+        public override void Write(byte[] buffer, int offset, int count)
+        {
+            DoWriteAsync(this.socket.OutputStream, buffer, offset, count).Wait();
+        }
+
+        /// <summary>
+        /// When overridden in a derived class, gets a value indicating whether the current stream supports reading.
+        /// </summary>
+        /// <returns>
+        /// true if the stream supports reading; otherwise, false.
+        /// </returns>
+        /// <filterpriority>1</filterpriority>
+        public override bool CanRead
+        {
+            get
+            {
+                return true;
+            }
+        }
+
+        /// <summary>
+        /// When overridden in a derived class, gets a value indicating whether the current stream supports seeking.
+        /// </summary>
+        /// <returns>
+        /// true if the stream supports seeking; otherwise, false.
+        /// </returns>
+        /// <filterpriority>1</filterpriority>
+        public override bool CanSeek
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// When overridden in a derived class, gets a value indicating whether the current stream supports writing.
+        /// </summary>
+        /// <returns>
+        /// true if the stream supports writing; otherwise, false.
+        /// </returns>
+        /// <filterpriority>1</filterpriority>
+        public override bool CanWrite
+        {
+            get
+            {
+                return true;
+            }
+        }
+
+        /// <summary>
+        /// When overridden in a derived class, gets the length in bytes of the stream.
+        /// </summary>
+        /// <returns>
+        /// A long value representing the length of the stream in bytes.
+        /// </returns>
+        /// <exception cref="T:System.NotSupportedException">A class derived from Stream does not support seeking. </exception>
+        /// <exception cref="T:System.ObjectDisposedException">Methods were called after the stream was closed. </exception>
+        /// <filterpriority>1</filterpriority>
+        public override long Length
+        {
+            get
+            {
+                throw new NotSupportedException();
+            }
+        }
+
+        /// <summary>
+        /// When overridden in a derived class, gets or sets the position within the current stream.
+        /// </summary>
+        /// <returns>
+        /// The current position within the stream.
+        /// </returns>
+        /// <exception cref="T:System.IO.IOException">An I/O error occurs. </exception>
+        /// <exception cref="T:System.NotSupportedException">The stream does not support seeking. </exception>
+        /// <exception cref="T:System.ObjectDisposedException">Methods were called after the stream was closed. </exception>
+        /// <filterpriority>1</filterpriority>
+        public override long Position
+        {
+            get
+            {
+                throw new NotSupportedException();
+            }
+            set
+            {
+                throw new NotSupportedException();
+            }
+        }
+
+        private async Task InitializeNetworkStream(string host, int port, bool useTls)
+        {
+            await this.socket.ConnectAsync(new HostName(host), port.ToString(CultureInfo.InvariantCulture));
 
             if (useTls)
             {
-                await
-                    this.socket.UpgradeToSslAsync(SocketProtectionLevel.Tls10, new HostName(host))
-                        .AsTask()
-                        .ConfigureAwait(false);
+                await this.socket.UpgradeToSslAsync(SocketProtectionLevel.Tls10, new HostName(host));
             }
+        }
 
-            this.networkStream = this.socket.OutputStream.AsStreamForWrite();
-            this.handle.Set();
+        private static async Task<int> DoReadAsync(IInputStream stream, byte[] buffer, int offset, int count)
+        {
+            try
+            {
+                using (var reader = new DataReader(stream))
+                {
+                    await reader.LoadAsync((uint)count);
+                    var length = Math.Min((int)reader.UnconsumedBufferLength, count);
+                    var tmp = offset == 0 && length == count
+                                  ? buffer
+                                  : new ArraySegment<byte>(buffer, offset, length).Array;
+                    reader.ReadBytes(tmp);
+                    reader.DetachStream();
+                    return length;
+                }
+            }
+            catch
+            {
+                return 0;
+            }
+        }
+
+        private static async Task DoWriteAsync(IOutputStream stream, byte[] buffer, int offset, int count)
+        {
+            try
+            {
+                using (var writer = new DataWriter(stream))
+                {
+                    var tmp = offset == 0 ? buffer : new ArraySegment<byte>(buffer, offset, count).Array;
+                    writer.WriteBytes(tmp);
+                    await writer.StoreAsync();
+                    writer.DetachStream();
+                }
+            }
+            catch (Exception e)
+            {
+                throw new IOException("Socket write failure.", e.InnerException ?? e);
+            }
         }
 
         #endregion
