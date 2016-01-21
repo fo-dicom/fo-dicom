f01cbb1d3c4d347180f5b41b3cd4ad8920e84af9
Anders Gustafsson <anders@cureos.com>
2016-01-21 09:52:18 +0100

    Unit test to verify fix. Issue #179

---
+++ DICOM.Tests/Bugs/GH179.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) 2012-2016 fo-dicom contributors.
+// Licensed under the Microsoft Public License (MS-PL).
+
+namespace Dicom.Bugs
+{
+    using Xunit;
+
+    [Collection("General")]
+    public class GH179
+    {
+        [Theory]
+        [InlineData(@".\Test Data\GH179.dcm")]
+        public void DicomFile_Contains_CommentsOnRadiationDoseIncluded(string fileName)
+        {
+            var file = DicomFile.Open(fileName);
+            var actual = file.Dataset.Contains(DicomTag.CommentsOnRadiationDose);
+            Assert.True(actual);
+        }
+    }
+}
