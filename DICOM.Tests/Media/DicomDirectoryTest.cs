298f7e0bbb5e92902629e7e0d3043b9ec4e7c116
Anders Gustafsson <anders@cureos.com>
2016-02-26 16:21:39 +0100

    Improved file layouts. Issue #181

--- DICOM.Tests/Media/DicomDirectoryTest.cs
+++ DICOM.Tests/Media/DicomDirectoryTest.cs
@@ -1,10 +1,9 @@
 ﻿// Copyright (c) 2012-2016 fo-dicom contributors.
 // Licensed under the Microsoft Public License (MS-PL).
 
-using System.IO;
-
 namespace Dicom.Media
 {
+    using System.IO;
     using System.Threading.Tasks;
 
     using Xunit;
