381b8180f03f603b643b61183cd2f7c81746fc9b
Anders Gustafsson <anders@cureos.com>
2016-04-20 15:17:15 +0200

    Implemented tests to identify errors. Issue #262

---
+++ DICOM.Tests/DicomFileMetaInformationTests.cs
@@ -0,0 +1,38 @@
+﻿// Copyright (c) 2012-2016 fo-dicom contributors.
+// Licensed under the Microsoft Public License (MS-PL).
+
+namespace Dicom
+{
+    using Xunit;
+
+    public class DicomFileMetaInformationTests
+    {
+        [Fact]
+        public void ImplementationVersionName_GetterWhenAttributeMissing_ReturnsNull()
+        {
+            var metaInfo =
+                new DicomFileMetaInformation(
+                    new DicomDataset(
+                        new DicomUniqueIdentifier(DicomTag.SOPClassUID, DicomUID.SecondaryCaptureImageStorage),
+                        new DicomUniqueIdentifier(DicomTag.SOPInstanceUID, "1.2.3")));
+            metaInfo.Remove(DicomTag.ImplementationVersionName);
+
+            var exception = Record.Exception(() => { Assert.Null(metaInfo.ImplementationVersionName); });
+            Assert.Null(exception);
+        }
+
+        [Fact]
+        public void SourceApplicationEntityTitle_GetterWhenAttributeMissing_ReturnsNull()
+        {
+            var metaInfo =
+                new DicomFileMetaInformation(
+                    new DicomDataset(
+                        new DicomUniqueIdentifier(DicomTag.SOPClassUID, DicomUID.SecondaryCaptureImageStorage),
+                        new DicomUniqueIdentifier(DicomTag.SOPInstanceUID, "1.2.3")));
+            metaInfo.Remove(DicomTag.SourceApplicationEntityTitle);
+
+            var exception = Record.Exception(() => { Assert.Null(metaInfo.SourceApplicationEntityTitle); });
+            Assert.Null(exception);
+        }
+    }
+}
