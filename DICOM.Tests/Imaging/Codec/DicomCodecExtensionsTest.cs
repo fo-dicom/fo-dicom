2547d06d50104b9d426f5d7fbecb98f2a581aa9f
Anders Gustafsson <anders@cureos.com>
2016-05-10 09:46:14 +0200

    Added unit test to verify fix. Issue #277

---
+++ DICOM.Tests/Imaging/Codec/DicomCodecExtensionsTest.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) 2012-2016 fo-dicom contributors.
+// Licensed under the Microsoft Public License (MS-PL).
+
+using Xunit;
+
+namespace Dicom.Imaging.Codec
+{
+    public class DicomCodecExtensionsTest
+    {
+        [Fact]
+        public void ChangeTransferSyntax_FromJ2KToJPEGWithParameters_DoesNotThrow()
+        {
+            var file = DicomFile.Open(@".\Test Data\CT1_J2KI");
+            var exception =
+                Record.Exception(
+                    () =>
+                    file.ChangeTransferSyntax(DicomTransferSyntax.JPEGProcess2_4, new DicomJpegParams { Quality = 50 }));
+            Assert.Null(exception);
+        }
+    }
+}
