2034add6e10f80d8464e5e10690ef643efc100f0
Anders Gustafsson <anders@cureos.com>
2016-05-13 14:24:31 +0200

    Added placeholders for Unity IImage implementation.

---
+++ DICOM/Imaging/UnityImage.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) 2012-2016 fo-dicom contributors.
+// Licensed under the Microsoft Public License (MS-PL).
+
+namespace Dicom.Imaging
+{
+    /// <summary>
+    /// Evaluation release placeholder for <see cref="IImage"/> implementation.
+    /// </summary>
+    public class UnityImage
+    {
+        #region CONSTRUCTORS
+
+        /// <summary>
+        /// Prevents a default instance of the <see cref="UnityImage"/> class from being created.
+        /// </summary>
+        private UnityImage()
+        {
+        }
+
+        #endregion
+    }
+}
