658412a0554c481dea6e88047576b6c790374d9c
Anders Gustafsson <anders@cureos.com>
2016-05-26 14:12:10 +0200

    Added unit tests to verify bug fixes. Issue #298

---
+++ Tests/Network/DicomNActionResponseTest.cs
@@ -0,0 +1,40 @@
+﻿// Copyright (c) 2012-2016 fo-dicom contributors.
+// Licensed under the Microsoft Public License (MS-PL).
+
+namespace Dicom.Network
+{
+    using Xunit;
+
+    public class DicomNActionResponseTest
+    {
+        #region Unit tests
+
+        [Fact]
+        public void ActionTypeIDGetter_ResponseCreatedFromRequest_DoesNotThrow()
+        {
+            var request = new DicomNActionRequest(
+                DicomUID.BasicFilmSessionSOPClass,
+                new DicomUID("1.2.3", null, DicomUidType.SOPInstance),
+                1);
+            var response = new DicomNActionResponse(request, DicomStatus.Success);
+
+            var exception = Record.Exception(() => response.ActionTypeID);
+            Assert.Null(exception);
+        }
+
+        [Fact]
+        public void MessageIDGetter_ResponseCreatedFromRequest_DoesNotThrow()
+        {
+            var request = new DicomNActionRequest(
+                DicomUID.BasicFilmSessionSOPClass,
+                new DicomUID("1.2.3", null, DicomUidType.SOPInstance),
+                1);
+            var response = new DicomNActionResponse(request, DicomStatus.Success);
+
+            var exception = Record.Exception(() => response.MessageID);
+            Assert.Null(exception);
+        }
+
+        #endregion
+    }
+}
