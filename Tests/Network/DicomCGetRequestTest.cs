e5ad0d27309eb08e5c733e9c141dce6223599dbf
Anders Gustafsson <anders@cureos.com>
2016-06-01 17:17:40 +0200

    Added unit tests to illustrate C-GET usage. Fixes #180

--- Tests/Network/DicomCGetRequestTest.cs
+++ Tests/Network/DicomCGetRequestTest.cs
@@ -3,30 +3,84 @@
 
 namespace Dicom.Network
 {
+    using System.Threading;
+
     using Xunit;
 
     public class DicomCGetRequestTest
     {
         [Fact(Skip = "Require running Q/R SCP containing CT-MONO2-16-ankle image")]
-        public void TestCGetWorkflow()
+        public void DicomCGetRequest_OneImageInSeries_Received()
         {
             var client = new DicomClient();
-            client.NegotiateAsyncOps(16, 16);
+            client.NegotiateAsyncOps(2, 1);
 
-            var pc = new DicomPresentationContext(3, DicomUID.SecondaryCaptureImageStorage, false, true);
-            pc.AddTransferSyntax(DicomTransferSyntax.ImplicitVRLittleEndian);
-            client.AdditionalPresentationContexts.Add(pc);
+            var pcs = DicomPresentationContext.GetScpRolePresentationContextsFromStorageUids(
+                DicomStorageCategory.Image,
+                DicomTransferSyntax.ExplicitVRLittleEndian,
+                DicomTransferSyntax.ImplicitVRLittleEndian,
+                DicomTransferSyntax.ImplicitVRBigEndian);
+            client.AdditionalPresentationContexts.AddRange(pcs);
+
+            DicomDataset dataset = null;
+            client.OnCStoreRequest = request =>
+                {
+                    dataset = request.Dataset;
+                    return new DicomCStoreResponse(request, DicomStatus.Success);
+                };
 
             var get = new DicomCGetRequest(
                 "1.2.840.113619.2.1.1.322987881.621.736170080.681",
                 "1.2.840.113619.2.1.2411.1031152382.365.736169244");
 
+            var handle = new ManualResetEventSlim();
             get.OnResponseReceived = (request, response) =>
             {
-                Assert.Equal(0, response.Failures);
+                handle.Set();
             };
             client.AddRequest(get);
             client.Send("localhost", 11112, false, "SCU", "COMMON");
+            handle.Wait();
+
+            Assert.Equal("RT ANKLE", dataset.Get<string>(DicomTag.StudyDescription));
+        }
+
+        [Fact(Skip = "Require running Q/R SCP containing specific study")]
+        public void DicomCGetRequest_PickCTImagesInStudy_OnlyCTImagesRetrieved()
+        {
+            var client = new DicomClient();
+            client.NegotiateAsyncOps(2, 1);
+
+            var pc = DicomPresentationContext.GetScpRolePresentationContext(DicomUID.CTImageStorage);
+            client.AdditionalPresentationContexts.Add(pc);
+
+            var counter = 0;
+            var locker = new object();
+            client.OnCStoreRequest = request =>
+            {
+                lock (locker)
+                {
+                    ++counter;
+                }
+
+                return new DicomCStoreResponse(request, DicomStatus.Success);
+            };
+
+            var get = new DicomCGetRequest("1.2.840.113619.2.55.3.2609388324.145.1222836278.84");
+
+            var handle = new ManualResetEventSlim();
+            get.OnResponseReceived = (request, response) =>
+            {
+                if (response.Remaining == 0)
+                {
+                    handle.Set();
+                }
+            };
+            client.AddRequest(get);
+            client.Send("localhost", 11112, false, "SCU", "COMMON");
+            handle.Wait();
+
+            Assert.Equal(140, counter);
         }
     }
 }
