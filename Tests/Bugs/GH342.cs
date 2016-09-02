c5cfd08c3798f1c5406014fe07af21c751de87ea
Anders Gustafsson <anders@cureos.com>
2016-09-02 17:14:35 +0200

    Added unit tests to verify fix. Issue #342

---
+++ Tests/Bugs/GH342.cs
@@ -0,0 +1,33 @@
+﻿// Copyright (c) 2012-2016 fo-dicom contributors.
+// Licensed under the Microsoft Public License (MS-PL).
+
+namespace Dicom.Bugs
+{
+    using Xunit;
+
+    [Collection("General")]
+    public class GH342
+    {
+        #region Unit tests
+
+        [Fact]
+        public void Open_EmptyItemsSequence_CanReadBefore()
+        {
+            var file = DicomFile.Open(@"Test Data\GH342.dcm");
+            const string expected = "RIGHT_ON_LEFT";
+            var actual = file.Dataset.Get<string>(new DicomTag(0x01f1, 0x32, "ELSCINT"));
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Open_EmptyItemsSequence_CanReadAfter()
+        {
+            var file = DicomFile.Open(@"Test Data\GH342.dcm");
+            const string expected = "3.5";
+            var actual = file.Dataset.Get<string>(new DicomTag(0x07a1, 0x10, "ELSCINT"));
+            Assert.Equal(expected, actual);
+        }
+
+        #endregion
+    }
+}
