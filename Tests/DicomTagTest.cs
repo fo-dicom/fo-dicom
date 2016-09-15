84e8a6120bac03cdddf46db4b12542d077ffcd1b
Anders Gustafsson <anders@cureos.com>
2016-09-15 11:39:36 +0200

    Complementary DicomTag tests. Issue #319

--- Tests/DicomTagTest.cs
+++ Tests/DicomTagTest.cs
@@ -1,6 +1,8 @@
 ﻿// Copyright (c) 2012-2016 fo-dicom contributors.
 // Licensed under the Microsoft Public License (MS-PL).
 
+using System.Collections.Generic;
+
 namespace Dicom
 {
     using System;
@@ -14,6 +16,8 @@ namespace Dicom
     [Collection("General")]
     public class DicomTagTest
     {
+        #region Unit tests
+
         /// <summary>
         ///     A test for ToString
         /// </summary>
@@ -30,5 +34,47 @@ namespace Dicom
             actual = target.ToString(format, formatProvider);
             Assert.Equal(expected, actual);
         }
+
+        [Theory]
+        [MemberData(nameof(DoesContain))]
+        public void Contains_Existing_ReturnsTrue(DicomTag tag)
+        {
+            var dataset = new DicomDataset { new DicomUnknown(tag) };
+            Assert.True(dataset.Contains(tag));
+        }
+
+        [Theory]
+        [MemberData(nameof(DoesNotContain))]
+        public void Contains_NonExisting_ReturnsFalse(DicomTag datasetTag, DicomTag containsTag)
+        {
+            var dataset = new DicomDataset { new DicomUnknown(datasetTag) };
+            Assert.False(dataset.Contains(containsTag));
+        }
+
+        [Fact]
+        public void Contains_SamePrivateTagsDifferentPrivateCreator_ReturnsTrue()
+        {
+            var dataset = new DicomDataset { new DicomUnknown(new DicomTag(0x3005, 0x3025, "PRIVATE")) };
+            Assert.True(dataset.Contains(new DicomTag(0x3005, 0x1525, "PRIVATE")));
+        }
+
+        #endregion
+
+        #region Test data
+
+        public static readonly IEnumerable<object[]> DoesContain = new[]
+                                                                 {
+                                                                     new[] { DicomTag.BitsAllocated },
+                                                                     new[] { DicomTag.AcquisitionContextSequence },
+                                                                     new[] { new DicomTag(0x3005, 0x25, "PRIVATE") }
+                                                                 };
+
+        public static readonly IEnumerable<object[]> DoesNotContain = new[]
+                                                                 {
+                                                                     new[] { DicomTag.BitsAllocated, DicomTag.BitsStored },
+                                                                     new[] { new DicomTag(0x3005, 0x25, "PRIVATE"), new DicomTag(0x3005, 0x26, "PRIVATE") }
+                                                                 };
+
+        #endregion
     }
 }
