5ae3fcccbae655aa8235e5cd275e01c0f664bae9
Anders Gustafsson <anders@cureos.com>
2016-12-19 17:59:55 +0100

    Added unit test to confirm issue. Issue #364

---
+++ Tests/Bugs/GH364.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) 2012-2016 fo-dicom contributors.
+// Licensed under the Microsoft Public License (MS-PL).
+
+using Xunit;
+
+namespace Dicom.Bugs
+{
+    [Collection("General")]
+    public class GH364
+    {
+        [Theory]
+        [InlineData(@".\Test Data\GH364.dcm")]
+        public void DicomFile_Contains_TagBeyond00185020(string fileName)
+        {
+            var file = DicomFile.Open(fileName);
+            var actual = file.Dataset.Contains(DicomTag.StudyInstanceUID);
+            Assert.True(actual);
+        }
+    }
+}
