5b918219a180a938f170892561d004c0df3d59e7
Anders Gustafsson <anders@cureos.com>
2017-02-09 12:54:15 +0100

    Added unit test project for Universal. Issue #435

---
+++ Tests/Universal/Bugs/GH435.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) 2012-2017 fo-dicom contributors.
+// Licensed under the Microsoft Public License (MS-PL).
+
+using System.Threading.Tasks;
+
+using Dicom.Helpers;
+
+using Xunit;
+
+namespace Dicom.Bugs
+{
+    public class GH435
+    {
+        [Fact]
+        public async Task DicomFileOpen_MissingSequenceDeclaration_CanReadBeyond()
+        {
+            using (var classicStream = await ApplicationContent.GetStreamAsync("Data/GH364.dcm"))
+            {
+                var dicomFile = await DicomFile.OpenAsync(classicStream);
+                var actual = dicomFile.Dataset.Contains(DicomTag.StudyInstanceUID);
+                Assert.True(actual);
+            }
+        }
+    }
+}
