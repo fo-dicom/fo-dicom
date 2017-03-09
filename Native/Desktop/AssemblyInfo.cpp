e328dee50457232c8de909b48005c33179b34f0b
Anders Gustafsson <anders@cureos.com>
2017-03-09 12:23:47 +0100

    Move desktop core and native projects to Platform and Native folders, respectively. Issue #469

--- Native/Desktop/AssemblyInfo.cpp
+++ Native/Desktop/AssemblyInfo.cpp
@@ -1,7 +1,7 @@
 // Copyright (c) 2012-2017 fo-dicom contributors.
 // Licensed under the Microsoft Public License (MS-PL).
 
-#include "../SharedAssemblyInfo.cs"
+#include "../../SharedAssemblyInfo.cs"
 
 ; // terminate cs "include"
 
