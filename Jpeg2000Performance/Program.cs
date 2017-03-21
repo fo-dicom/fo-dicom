b426af6096909edae9d91cf85eec68561c94ada4
Anders Gustafsson <anders@cureos.com>
2017-03-21 19:24:54 +0100

    Test program for measuring JPEG 2000 decoding performance.

---
+++ Jpeg2000Performance/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+using Dicom;
+using Dicom.Imaging;
+
+namespace Jpeg2000Performance
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var path = args.Length > 0 ? args[0] : @"C:\Users\ander\Documents\Data\DICOM\compsamples_j2k\IMAGES";
+
+            var files = Directory.GetFiles(path, "*", SearchOption.AllDirectories);
+
+            var stopwatch = new Stopwatch();
+
+            foreach (var file in files)
+            {
+                try
+                {
+                    stopwatch.Start();
+                    var dcm = DicomFile.Open(file);
+                    stopwatch.Stop();
+                    var reading = stopwatch.ElapsedMilliseconds;
+
+                    stopwatch.Reset();
+                    stopwatch.Start();
+                    var rendered = new DicomImage(dcm.Dataset).RenderImage();
+                    stopwatch.Stop();
+                    var rendering = stopwatch.ElapsedMilliseconds;
+
+                    stopwatch.Reset();
+                    stopwatch.Start();
+                    var bitmap = rendered.AsBitmap();
+                    stopwatch.Stop();
+                    var converting = stopwatch.ElapsedMilliseconds;
+
+                    Console.WriteLine($"{file}: {reading} ms, {rendering} ms, {converting} ms");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"Error for {file}: {e}");
+                }
+            }
+        }
+    }
+}
