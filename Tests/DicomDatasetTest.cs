9aebc755f2fb09cd91cd07f1fb5ff722e7f7fa7f
Rickard Holmberg <rickard.holmberg@raysearchlabs.com>
2017-03-27 17:03:44 +0200

    Failing test on adding private tags with correct VR

--- Tests/DicomDatasetTest.cs
+++ Tests/DicomDatasetTest.cs
@@ -321,9 +321,9 @@ namespace Dicom
             var ds        = new DicomDataset ( );
             var data      = new IO.Buffer.MemoryByteBuffer ( new byte[] { 255 } ); //dummy data
             var newSyntax = DicomTransferSyntax.DeflatedExplicitVRLittleEndian;
-            
+
             ds.AddOrUpdatePixelData ( DicomVR.OB, data );
-            
+
             Assert.Equal(DicomTransferSyntax.ExplicitVRLittleEndian, ds.InternalTransferSyntax);
 
             ds.AddOrUpdatePixelData ( DicomVR.OB, data, newSyntax );
@@ -341,6 +341,23 @@ namespace Dicom
 
             var array = ds.Get<string[]>(tag);
             Assert.Equal(0, array.Length);
+        }
+
+        [Fact]
+        public void Add_PrivateTag_ShouldBeAddedWithCorrectVR()
+        {
+            var privCreatorDictEntry = new DicomDictionaryEntry(new DicomTag(0x0011, 0x0010), "Private Creator", "PrivateCreator", DicomVM.VM_1, false, DicomVR.LO);
+            DicomDictionary.Default.Add(privCreatorDictEntry);
+
+            DicomPrivateCreator privateCreator1 = DicomDictionary.Default.GetPrivateCreator("TESTCREATOR1");
+            DicomDictionary privDict1 = DicomDictionary.Default[privateCreator1];
+
+            var dictEntry = new DicomDictionaryEntry(DicomMaskedTag.Parse("0011", "xx10"), "TestPrivTagName", "TestPrivTagKeyword", DicomVM.VM_1, false, DicomVR.CS);
+            privDict1.Add(dictEntry);
+
+            var ds = new DicomDataset();
+            ds.Add(dictEntry.Tag, "VAL1");
+            Assert.Equal(DicomVR.CS, ds.Get<DicomVR>(ds.GetPrivateTag(dictEntry.Tag)));
         }
 
         #endregion
