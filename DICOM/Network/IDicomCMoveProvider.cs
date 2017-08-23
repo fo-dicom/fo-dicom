f44dad117f831987f7a7d09a7744a394e10fed91
Anders Gustafsson <anders@cureos.com>
2017-08-23 12:11:49 +0200

    Updated API documentation. Issue #28

--- DICOM/Network/IDicomCMoveProvider.cs
+++ DICOM/Network/IDicomCMoveProvider.cs
@@ -5,8 +5,16 @@ using System.Collections.Generic;
 
 namespace Dicom.Network
 {
+    /// <summary>
+    /// Interface representing event handlers for DIMSE services applicable to C-MOVE SOP instances.
+    /// </summary>
     public interface IDicomCMoveProvider
     {
+        /// <summary>
+        /// Handler of C-MOVE request.
+        /// </summary>
+        /// <param name="request">C-MOVE request subject to handling.</param>
+        /// <returns>Collection of C-MOVE responses based on <paramref name="request"/>.</returns>
         IEnumerable<DicomCMoveResponse> OnCMoveRequest(DicomCMoveRequest request);
     }
 }
