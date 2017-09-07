1ed114dea588166f8c3b7e53adf5a927967093b2
Anders Gustafsson <anders@cureos.com>
2017-09-07 09:57:50 +0200

    Updated test to better comply with successful outcome. Issue #597

--- Tests/Desktop/Bugs/GH597.cs
+++ Tests/Desktop/Bugs/GH597.cs
@@ -20,10 +20,11 @@ namespace Dicom.Bugs
                 new DicomUniqueIdentifier(DicomTag.SOPInstanceUID, "1.2.4")
             };
 
-            const int length = 0x10000;
+            const uint expectedLength = 0x10000;
             var sb = new StringBuilder();
-            sb.Append('0', length);
-            dataset.Add(DicomTag.ContourData, sb.ToString());
+            sb.Append('0', (int)expectedLength);
+            var expectedValue = sb.ToString();
+            dataset.Add(DicomTag.ContourData, expectedValue);
 
             using (var stream = new MemoryStream())
             {
@@ -31,14 +32,17 @@ namespace Dicom.Bugs
                 file.Save(stream);
 
                 stream.Seek(0, SeekOrigin.Begin);
+                var streamString = Encoding.UTF8.GetString(stream.GetBuffer());
+                Assert.True(streamString.Contains("UN"), "streamString.Contains('UN')");
+
                 var newDataset = DicomFile.Open(stream).Dataset;
                 var contourDataItem = (DicomElement)newDataset.ElementAt(2);
 
-                Assert.Equal((uint)length, contourDataItem.Length);
+                var actualLength = contourDataItem.Length;
+                var actualValue = contourDataItem.Get<string>(0);
 
-                var expected = DicomVR.UN;
-                var actual = contourDataItem.ValueRepresentation;
-                Assert.Equal(expected, actual);
+                Assert.Equal(expectedLength, actualLength);
+                Assert.Equal(expectedValue, actualValue);
             }
         }
     }
