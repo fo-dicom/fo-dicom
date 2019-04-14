114fcdda5481cb77eb7b5a227186791928be913f
Reinhard Gruber <reinhardgruber@aon.at>
2019-04-14 23:21:51 +0200

    Add unittest to test that decoding of JpegLossless in net core does not throw

---
+++ Tests/NetCore/Imaging/DicomJpegLossessTest.cs
@@ -0,0 +1,39 @@
+﻿using Dicom.Imaging.Codec;
+using System;
+using System.IO;
+using Xunit;
+
+namespace Dicom.Imaging
+{
+
+    public class DicomJpegLossessTest
+    {
+
+        [Theory]
+        [InlineData("GH538-jpeg1.dcm")]
+        [InlineData("GH538-jpeg14sv1.dcm")]
+        [InlineData("CR-MONO1-10-chest")]
+        public void DicomJpegLosses_DecodingAvailable(string filename)
+        {
+            ImageManager.SetImplementation(RawImageManager.Instance);
+            var file = DicomFile.Open(@".\Test Data\" + filename, FileReadOption.ReadAll);
+            var dcmImage = new DicomImage(file.Dataset);
+            var image = dcmImage.RenderImage();
+            Assert.NotNull(image);
+        }
+
+        [Fact]
+        public void DicomJpegLosses_EncodingShallThrow()
+        {
+            var file = DicomFile.Open(@".\Test Data\GH538-jpeg14sv1.dcm", FileReadOption.ReadAll);
+            var decoded = file.Dataset.Clone(DicomTransferSyntax.ImplicitVRLittleEndian);
+            var ex = Record.Exception(() =>
+            {
+                var encoded = decoded.Clone(DicomTransferSyntax.JPEGProcess14);
+            });
+            Assert.IsType<NotImplementedException>(ex);
+        }
+
+
+    }
+}
