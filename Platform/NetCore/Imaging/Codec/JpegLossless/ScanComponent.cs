cfd5f8dcc516ac74da9d9f709d99669de077682c
Reinhard Gruber <reinhardgruber@aon.at>
2019-04-14 22:22:26 +0200

    initial commit of JpegLosslessDecoder: a .Net port of JpegLosslessDecoder originally written in Java by Hermann Kroll

---
+++ Platform/NetCore/Imaging/Codec/JpegLossless/ScanComponent.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) 2012-2019 fo-dicom contributors.
+// Licensed under the Microsoft Public License (MS-PL).
+
+namespace Dicom.Imaging.Codec.JpegLossless
+{
+
+    internal class ScanComponent
+    {
+        public int AcTabSel { get; set; }
+
+        public int DcTabSel { get; set; }
+
+        public int ScanCompSel { get; set; }
+
+    }
+}
