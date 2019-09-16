763bc377d1206677829cb41388d93c27e09a9caf
Reinhard Gruber <gofal3@live.com>
2019-09-16 22:14:53 +0200

    fix issue #921: Photometric interpretation has been changed on any jpeg compression, but that shall only happen on jpeg baseline encodings.

--- Tests/Desktop/Imaging/Codec/DicomCodecExtensionsTest.cs
+++ Tests/Desktop/Imaging/Codec/DicomCodecExtensionsTest.cs
@@ -47,8 +47,14 @@ namespace Dicom.Imaging.Codec
         public void ChangeTransferSyntax_UpdatePhotometricInterpretationOnJPEGBaseline(string filename)
         {
             var file = DicomFile.Open(@".\Test Data\" + filename);
+
+            // when converting to JpegBaseline the Photometric interpretation has to be updated
             var newDataset = file.Dataset.Clone(DicomTransferSyntax.JPEGProcess2_4, new DicomJpegParams { ConvertColorspaceToRGB=true, Quality = 90,  SampleFactor = DicomJpegSampleFactor.SF422 });
             Assert.Equal("YBR_FULL_422", newDataset.GetString(DicomTag.PhotometricInterpretation));
+
+            // when converting to other jpeg coded that does not automatically convert to 8bitcolor the Photometric Interpretation has to stay the same.
+            var newProcess14 = file.Dataset.Clone(DicomTransferSyntax.JPEGProcess14SV1);
+            Assert.Equal(file.Dataset.GetString(DicomTag.PhotometricInterpretation), newProcess14.GetString(DicomTag.PhotometricInterpretation));
         }
 
 
