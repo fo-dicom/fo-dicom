80692b8706c497ead8a6bd8a67ec711f188e0a05
Reinhard Gruber <r.gruber@smartinmedia.com>
2020-02-27 23:30:54 +0100

    Add benchmark project. To have some values to compare when changing internal code.

---
+++ Tests/FO-DICOM.Benchmark/OpenFileBenchmarks.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Text;
+using BenchmarkDotNet.Attributes;
+using FellowOakDicom.Imaging;
+using FellowOakDicom.Serialization;
+using Newtonsoft.Json;
+
+namespace FellowOakDicom.Benchmark
+{
+    [MemoryDiagnoser]
+    public class OpenFileBenchmarks
+    {
+
+        private readonly string _rootpath;
+        private DicomDataset _dicomDir;
+
+        public OpenFileBenchmarks()
+        {
+            _rootpath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+        }
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            _dicomDir = DicomFile.Open(Path.Combine(_rootpath, "Data\\DICOMDIR")).Dataset;
+        }
+
+        [Benchmark]
+        public void OpenFile()
+        {
+            var file = DicomFile.Open(Path.Combine(_rootpath, "Data\\GH355.dcm"));
+        }
+
+        [Benchmark]
+        public void OpenFileReadAll()
+        {
+            var file = DicomFile.Open(Path.Combine(_rootpath, "Data\\GH355.dcm"), FileReadOption.ReadAll);
+        }
+
+        [Benchmark]
+        public void OpenFileAndRender()
+        {
+            var file = DicomFile.Open(Path.Combine(_rootpath, "Data\\GH355.dcm"));
+            var image = new DicomImage(file.Dataset);
+            var rendered = image.RenderImage();
+        }
+
+        [Benchmark]
+        public void JsonSerialization()
+        {
+            var json = JsonConvert.SerializeObject(_dicomDir, new JsonDicomConverter());
+            var reconstituatedDataset = JsonConvert.DeserializeObject<DicomDataset>(json, new JsonDicomConverter());
+        }
+
+    }
+}
---
+++ Tests/FO-DICOM.Benchmark/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using BenchmarkDotNet.Running;
+
+
+namespace FellowOakDicom.Benchmark
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<OpenFileBenchmarks>();
+
+            Console.Write(summary.AllRuntimes);
+        }
+    }
+}
