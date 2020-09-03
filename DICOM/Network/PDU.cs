6d932ad546c60e4963afa75092d6b345e48f1dc8
Alexander Moerman <alexander.moerman@gmail.com>
2020-09-03 12:33:18 +0200

    When reading a string from a PDU, read a fixed number of bytes, not a fixed number of characters. Depending on the encoding, these two things might not be the same

--- DICOM/Network/PDU.cs
+++ DICOM/Network/PDU.cs
@@ -5,7 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-
+using System.Text;
 using Dicom.IO;
 
 namespace Dicom.Network
@@ -37,12 +37,14 @@ namespace Dicom.Network
         /// Initializes new PDU for writing
         /// </summary>
         /// <param name="type">Type of PDU</param>
-        public RawPDU(byte type)
+        /// <param name="encoding">The encoding to use for encoding text</param>
+        public RawPDU(byte type, Encoding encoding = null)
         {
+            _encoding = encoding ?? DicomEncoding.Default;
             _type = type;
             _ms = new MemoryStream();
             _ms.Seek(0, SeekOrigin.Begin);
-            _bw = EndianBinaryWriter.Create(_ms, DicomEncoding.Default, Endian.Big);
+            _bw = EndianBinaryWriter.Create(_ms, _encoding, Endian.Big);
             _m16 = new Stack<long>();
             _m32 = new Stack<long>();
         }
@@ -51,10 +53,12 @@ namespace Dicom.Network
         /// Initializes new PDU reader from buffer
         /// </summary>
         /// <param name="buffer">Buffer</param>
-        public RawPDU(byte[] buffer)
+        /// <param name="encoding">The encoding to use for decoding text</param>
+        public RawPDU(byte[] buffer, Encoding encoding = null)
         {
+            _encoding = encoding ?? DicomEncoding.Default;
             _ms = new MemoryStream(buffer);
-            _br = EndianBinaryReader.Create(_ms, Endian.Big);
+            _br = EndianBinaryReader.Create(_ms, encoding, Endian.Big);
             _type = _br.ReadByte();
             _ms.Seek(6, SeekOrigin.Begin);
         }
@@ -191,18 +195,24 @@ namespace Dicom.Network
         }
 
         private readonly char[] _trimChars = { ' ', '\0' };
+        private readonly Encoding _encoding;
 
         /// <summary>
         /// Reads string from PDU
         /// </summary>
         /// <param name="name">Name of field</param>
-        /// <param name="count">Length of string</param>
+        /// <param name="numberOfBytes">Number of bytes to read</param>
         /// <returns>Field value</returns>
-        public string ReadString(string name, int count)
+        public string ReadString(string name, int numberOfBytes)
         {
-            CheckOffset(count, name);
-            var c = _br.ReadChars(count);
-            return new string(c).Trim(_trimChars);
+            var bytes = ReadBytes(name, numberOfBytes);
+
+#if PORTABLE
+            return _encoding.GetString(bytes, 0, bytes.Length).Trim(_trimChars);
+#else
+            return _encoding.GetString(bytes).Trim(_trimChars);
+#endif
+
         }
 
         /// <summary>
