1a237df911da67e14e626d2ace1829642d35cf1d
Reinhard Gruber <r.gruber@smartinmedia.com>
2020-09-25 22:02:30 +0200

    Fix handling of MaximumPDULength: Association.MaximumPDULength was initialized with the value from option, but then always overwritten by the value sent by the other service in AssociationRequestAC. - Option is assigned to Association.Option, so Association.Option.MaximumPDULength always holds the configured value, while the parsed value from the remote service is stored in Association.MaximumPDULength

--- DICOM/Network/PDU.cs
+++ DICOM/Network/PDU.cs
@@ -486,7 +486,7 @@ namespace Dicom.Network
             pdu.Write("Item-Type", 0x51);
             pdu.Write("Reserved", 0x00);
             pdu.Write("Item-Length", (ushort)0x0004);
-            pdu.Write("Max PDU Length", _assoc.MaximumPDULength);
+            pdu.Write("Max PDU Length", _assoc.Options?.MaxPDULength ?? DicomServiceOptions.Default.MaxPDULength);
 
             // Implementation Class UID
             pdu.Write("Item-Type", 0x52);
@@ -805,7 +805,7 @@ namespace Dicom.Network
             pdu.Write("Item-Type", 0x51);
             pdu.Write("Reserved", 0x00);
             pdu.Write("Item-Length", (ushort)0x0004);
-            pdu.Write("Max PDU Length", _assoc.MaximumPDULength);
+            pdu.Write("Max PDU Length", _assoc.Options?.MaxPDULength ?? DicomServiceOptions.Default.MaxPDULength);
 
             // Implementation Class UID
             pdu.Write("Item-Type", 0x52);
@@ -951,8 +951,7 @@ namespace Dicom.Network
                         }
                         else if (ut == 0x52)
                         {
-                            _assoc.RemoteImplementationClassUID =
-                                DicomUID.Parse(raw.ReadString("Implementation Class UID", ul));
+                            _assoc.RemoteImplementationClassUID = DicomUID.Parse(raw.ReadString("Implementation Class UID", ul));
                         }
                         else if (ut == 0x53)
                         {
