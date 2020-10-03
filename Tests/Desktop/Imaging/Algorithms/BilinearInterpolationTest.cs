2a254cd1e002e33fd1c6e24aa50f1e17c387c876
Lydon Chandra <lydonch@gmail.com>
2020-10-03 16:35:09 +0800

    add BilinearInterpolation RescaleGrayscale test for non-uniform input array

--- Tests/Desktop/Imaging/Algorithms/BilinearInterpolationTest.cs
+++ Tests/Desktop/Imaging/Algorithms/BilinearInterpolationTest.cs
@@ -6,7 +6,7 @@ namespace Dicom.Imaging.Algorithms
     public class BilinearInterpolationTest
     {
         [Fact]
-        public void RescaleGrayscale2x2()
+        public void RescaleGrayscale2x2_to_4x4()
         {
             byte[] input = { 0, 0, 10, 10 };
 
@@ -29,7 +29,56 @@ namespace Dicom.Imaging.Algorithms
         }
 
         [Fact]
-        public void RescaleGrayscale4x4()
+        public void RescaleGrayscale3x3_to_5x5()
+        {
+            byte[] input =
+            {
+                0, 0, 0,
+                5, 5, 5,
+                10, 10, 10
+            };
+
+            const int inputWidth = 3,
+                inputHeight = inputWidth;
+
+            const int outputWidth = 5, outputHeight = 5;
+
+            byte[] output = BilinearInterpolation.RescaleGrayscale(input, inputWidth, inputHeight, outputWidth, outputHeight);
+
+            Assert.Equal(
+                new byte[]
+                {
+                    0, 0, 0, 0, 0,
+                    3, 3, 3, 3, 3,
+                    6, 6, 6, 6, 6,
+                    9, 9, 9, 9, 9,
+                    10, 10, 10, 10, 10
+                },
+                output);
+
+
+            input = new byte [] {
+                0, 1, 2,
+                3, 4, 5,
+                6, 7, 8
+            };
+
+            output = BilinearInterpolation.RescaleGrayscale(input, inputWidth, inputHeight, outputWidth, outputHeight);
+
+            Assert.Equal(
+                new byte[]
+                {
+                    0, 0, 1, 1, 2,
+                    1, 2, 3, 3, 3,
+                    3, 4, 4, 5, 5,
+                    5, 6, 6, 7, 7,
+                    6, 6, 7, 7, 8
+                },
+                output);
+        }
+
+        [Fact]
+        public void RescaleGrayscale4x4_to_8x8()
         {
             byte[] input =
             {
