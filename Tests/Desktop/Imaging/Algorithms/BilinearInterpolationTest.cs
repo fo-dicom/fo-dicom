c476940a785c97f03d017b6a37d385f38bbd0b7b
Lydon Chandra <lydonch@gmail.com>
2020-10-03 17:05:23 +0800

    add non uniform input array for RescaleGrayscale4x4_to_8x8 test

--- Tests/Desktop/Imaging/Algorithms/BilinearInterpolationTest.cs
+++ Tests/Desktop/Imaging/Algorithms/BilinearInterpolationTest.cs
@@ -56,6 +56,7 @@ namespace Dicom.Imaging.Algorithms
                 },
                 output);
 
+            // non uniform input array
 
             input = new byte [] {
                 0, 1, 2,
@@ -98,10 +99,10 @@ namespace Dicom.Imaging.Algorithms
             Assert.Equal(
                 new byte[]
                 {
-                    0, 0, 0, 0, 0, 0, 0, 0,
-                    2, 2, 2, 2, 2, 2, 2, 2,
-                    5, 5, 5, 5, 5, 5, 5, 5,
-                    7, 7, 7, 7, 7, 7, 7, 7,
+                    0,  0,  0,  0,  0,  0,  0,  0,
+                    2,  2,  2,  2,  2,  2,  2,  2,
+                    5,  5,  5,  5,  5,  5,  5,  5,
+                    7,  7,  7,  7,  7,  7,  7,  7,
                     10, 10, 10, 10, 10, 10, 10, 10,
                     12, 12, 12, 12, 12, 12, 12, 12,
                     15, 15, 15, 15, 15, 15, 15, 15,
@@ -109,6 +110,31 @@ namespace Dicom.Imaging.Algorithms
                 },
                 output);
 
+            // non uniform input array
+
+            input = new byte[]
+            {
+                0,  1,  2,  3,
+                4,  5,  6,  7,
+                8,  9,  10, 11,
+                12, 13, 14, 15
+            };
+
+            output = BilinearInterpolation.RescaleGrayscale(input, inputWidth, inputHeight, outputWidth, outputHeight);
+
+            Assert.Equal(
+                new byte[]
+                {
+                    0,  0,  1,  1,  2,  2,  3,  3,
+                    2,  2,  3,  3,  4,  4,  5,  5,
+                    4,  4,  5,  5,  6,  6,  7,  7,
+                    6,  6,  7,  7,  8,  8,  9,  9,
+                    8,  8,  9,  9,  10, 10, 11, 11,
+                    10, 10, 11, 11, 12, 12, 13, 13,
+                    12, 12, 13, 13, 14, 14, 15, 15,
+                    12, 12, 13, 13, 14, 14, 15, 15
+                },
+                output);
         }
 
     }
