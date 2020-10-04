5146001a26fc752770c28e8f327ac274bd689404
Reinhard Gruber <r.gruber@smartinmedia.com>
2020-10-04 11:13:13 +0200

    - Execute all benchmarks - Add deflated file to OpenFileBenchmarks

--- Tests/FO-DICOM.Benchmark/Program.cs
+++ Tests/FO-DICOM.Benchmark/Program.cs
@@ -1,25 +1,28 @@
 ﻿// Copyright (c) 2012-2020 fo-dicom contributors.
 // Licensed under the Microsoft Public License (MS-PL).
 
-using System;
 using BenchmarkDotNet.Running;
 
 
 namespace FellowOakDicom.Benchmark
 {
-    class Program
+    static class Program
     {
+
         static void Main(string[] args)
         {
-            //var summary2 = BenchmarkRunner.Run<ServerBenchmarks>();
-            //var summary = BenchmarkRunner.Run<OpenFileBenchmarks>();
+            // benchmark opening files
+            BenchmarkRunner.Run<OpenFileBenchmarks>();
 
-            //Console.Write(summary.Table.ToString());
-            //Console.Write(summary2.Table.ToString());
+            // bencharm instanciating a server
+            BenchmarkRunner.Run<ServerBenchmarks>();
 
-            // var summary3 = BenchmarkRunner.Run<JsonBenchmarks>();
-            var summary4 = BenchmarkRunner.Run<ParseDatasetBenchmark>();
-            Console.Write(summary4.Table.ToString());
+            // benchmark json serialization and deserialization
+            BenchmarkRunner.Run<JsonBenchmarks>();
+
+            // benchmark parsing datasets
+            BenchmarkRunner.Run<ParseDatasetBenchmark>();
         }
+
     }
 }
