dbd92140a24c17290ebb9cffda52f2940799e2e4
Reinhard Gruber <r.gruber@smartinmedia.com>
2021-03-01 18:56:06 +0100

    Add unittest to verify fallback encoding behavior

--- Tests/FO-DICOM.Tests/DicomEncodingTest.cs
+++ Tests/FO-DICOM.Tests/DicomEncodingTest.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) 2012-2021 fo-dicom contributors.
 // Licensed under the Microsoft Public License (MS-PL).
 
+using System.IO;
 using System.Text;
 using Xunit;
 
@@ -43,6 +44,32 @@ namespace FellowOakDicom.Tests
             var actual = DicomEncoding.GetCharset(encoding);
 
             Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void FallbackEncoding()
+        {
+            var ds = new DicomDataset
+            {
+                { DicomTag.SOPClassUID, DicomUID.SecondaryCaptureImageStorage },
+                { DicomTag.SOPInstanceUID, "1.2.345.67890" }
+            };
+            ds.AddOrUpdate(new DicomPersonName(DicomTag.PatientName, DicomEncoding.Default, new FellowOakDicom.IO.Buffer.MemoryByteBuffer(DicomEncoding.GetEncoding("ISO_IR 100").GetBytes("Hölzl^Günther"))));
+            var firstFile = new DicomFile(ds);
+            var filestream = new MemoryStream();
+            firstFile.Save(filestream);
+
+            filestream.Flush();
+
+            filestream.Position = 0;
+            var secondFile = DicomFile.Open(filestream);
+            // the dataset has be read with Default enconding ASCII and therefore "ö" and "ü" should not be recognized
+            Assert.Equal("H?lzl^G?nther", secondFile.Dataset.GetString(DicomTag.PatientName));
+
+            filestream.Position = 0;
+            var thirdFile = DicomFile.Open(filestream, DicomEncoding.GetEncoding("ISO_IR 100"));
+            // if reading with middle european encoding as fallback default the "ö" and "ü" should be recognized
+            Assert.Equal("Hölzl^Günther", thirdFile.Dataset.GetString(DicomTag.PatientName));
         }
     }
 }
