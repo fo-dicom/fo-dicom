99a04b09480a8067b3f6d91e8cd5c13781547196
Alexander Moerman <a.moerman@dobcomed.com>
2021-10-12 16:40:27 +0200

    Fix unit test where server is disposed: IO exceptions do not necessarily inherit from DicomNetworkException, and I am unsure if we always want to wrap these

--- Tests/FO-DICOM.Tests/Network/Client/DicomClientTest.cs
+++ Tests/FO-DICOM.Tests/Network/Client/DicomClientTest.cs
@@ -1358,7 +1358,7 @@ namespace FellowOakDicom.Tests.Network.Client
                 {
                     await client.SendAsync(cancellation.Token, DicomClientCancellationMode.ImmediatelyAbortAssociation).ConfigureAwait(false);
                 }
-                catch(DicomNetworkException) { /* Ignore */ }
+                catch(Exception) { /* Ignore */ }
 
                 Assert.False(cancellation.IsCancellationRequested);
             }
