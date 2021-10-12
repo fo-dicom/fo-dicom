4840ac55ffeb0b3cd5f2fdbdd1a36bd18b18758c
Alexander Moerman <a.moerman@dobcomed.com>
2021-10-12 21:37:29 +0200

    Add extensions for C-FIND, C-STORE and C-MOVE

--- FO-DICOM.Core/Network/Client/Advanced/Association/AdvancedDicomClientAssociationExtensions.cs
+++ FO-DICOM.Core/Network/Client/Advanced/Association/AdvancedDicomClientAssociationExtensions.cs
@@ -1,6 +1,8 @@
 ﻿// Copyright (c) 2012-2021 fo-dicom contributors.
 // Licensed under the Microsoft Public License (MS-PL).
 
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -12,21 +14,76 @@ namespace FellowOakDicom.Network.Client.Advanced.Association
         /// Sends a C-ECHO request over the provided association
         /// </summary>
         /// <param name="association">The association with another AE to send to the request to</param>
-        /// <param name="dicomRequest">The echo request to send</param>
+        /// <param name="request">The request to send</param>
         /// <param name="cancellationToken">The token that cancels the request</param>
-        /// <returns></returns>
-        /// <exception cref="DicomNetworkException"></exception>
-        public static async Task<DicomCEchoResponse> SendEchoRequestAsync(this IAdvancedDicomClientAssociation association, DicomCEchoRequest dicomRequest,
+        /// <returns>A single C-ECHO response</returns>
+        public static async Task<DicomCEchoResponse> SendCEchoRequestAsync(
+            this IAdvancedDicomClientAssociation association, 
+            DicomCEchoRequest request,
             CancellationToken cancellationToken)
         {
-            await foreach (var response in association.SendRequestAsync(dicomRequest, cancellationToken).ConfigureAwait(false))
+            await foreach (var response in association.SendRequestAsync(request, cancellationToken).ConfigureAwait(false))
             {
                 return response as DicomCEchoResponse;
             }
 
-            throw new DicomNetworkException($"C-ECHO request {dicomRequest.MessageID} failed: no DICOM events received");
+            throw new DicomNetworkException($"C-ECHO request {request} failed: not a single DICOM response received");
         }
+        
+        /// <summary>
+        /// Sends a C-STORE request over the provided association
+        /// </summary>
+        /// <param name="association">The association with another AE to send to the request to</param>
+        /// <param name="request">The request to send</param>
+        /// <param name="cancellationToken">The token that cancels the request</param>
+        /// <returns>A single C-STORE response</returns>
+        public static async Task<DicomCStoreResponse> SendCStoreRequestAsync(
+            this IAdvancedDicomClientAssociation association, 
+            DicomCStoreRequest request,
+            CancellationToken cancellationToken)
+        {
+            await foreach (var response in association.SendRequestAsync(request, cancellationToken).ConfigureAwait(false))
+            {
+                return response as DicomCStoreResponse;
+            }
 
-        // TODO Add C-STORE, C-FIND, C-MOVE and C-ECHO
+            throw new DicomNetworkException($"C-STORE request {request} failed: not a single DICOM response received");
+        }
+        
+        /// <summary>
+        /// Sends a C-FIND request over the provided association
+        /// </summary>
+        /// <param name="association">The association with another AE to send to the request to</param>
+        /// <param name="request">The request to send</param>
+        /// <param name="cancellationToken">The token that cancels the request</param>
+        /// <returns>(Typically) One pending C-FIND response per SOP instance, and lastly one success C-FIND response</returns>
+        public static async IAsyncEnumerable<DicomCFindResponse> SendCFindRequestAsync(
+            this IAdvancedDicomClientAssociation association, 
+            DicomCFindRequest request,
+            [EnumeratorCancellation] CancellationToken cancellationToken)
+        {
+            await foreach (var response in association.SendRequestAsync(request, cancellationToken).ConfigureAwait(false))
+            {
+                yield return response as DicomCFindResponse;
+            }
+        }
+        
+        /// <summary>
+        /// Sends a C-FIND request over the provided association
+        /// </summary>
+        /// <param name="association">The association with another AE to send to the request to</param>
+        /// <param name="request">The request to send</param>
+        /// <param name="cancellationToken">The token that cancels the request</param>
+        /// <returns>(Typically) One pending C-MOVE response per stored SOP instance, and lastly one success C-MOVE response</returns>
+        public static async IAsyncEnumerable<DicomCMoveResponse> SendCMoveRequestAsync(
+            this IAdvancedDicomClientAssociation association, 
+            DicomCFindRequest request,
+            [EnumeratorCancellation] CancellationToken cancellationToken)
+        {
+            await foreach (var response in association.SendRequestAsync(request, cancellationToken).ConfigureAwait(false))
+            {
+                yield return response as DicomCMoveResponse;
+            }
+        }
     }
 }
