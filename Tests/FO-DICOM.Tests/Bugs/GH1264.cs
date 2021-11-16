e4ffa3b6da3d104340e1e93e611f45eb2aa4fa8f
Alexander Moerman <alexander.moerman@gmail.com>
2021-11-16 09:49:33 +0100

    Fix test, I should serialize the dataset instead of the file..

--- Tests/FO-DICOM.Tests/Bugs/GH1264.cs
+++ Tests/FO-DICOM.Tests/Bugs/GH1264.cs
@@ -11,7 +11,7 @@ namespace FellowOakDicom.Tests.Bugs
         public void OpenDicomFileFromStream()
         {
             // Arrange
-            var inputFile = TestData.Resolve("test_720.dcm");
+            var inputFile = TestData.Resolve("TestPattern_RGB.dcm");
             var outputFile = new FileInfo("./GH1264.dcm");
 
             outputFile.Delete();
@@ -23,18 +23,21 @@ namespace FellowOakDicom.Tests.Bugs
 
             ms.Seek(0, SeekOrigin.Begin);
 
-            var expectedDicomFile = DicomFile.Open(ms);
+            var dicomFileFromPath = DicomFile.Open(inputFile);
+            var dicomFileFromStream = DicomFile.Open(ms);
 
             // Act
-            expectedDicomFile.Save(outputFile.FullName);
+            dicomFileFromStream.Save(outputFile.FullName);
             var reopenedDicomFile = DicomFile.Open(outputFile.FullName);
 
             // Assert
             var jsonDicomConverter = new JsonDicomConverter(false, false);
-            var expected = JsonConvert.SerializeObject(expectedDicomFile, jsonDicomConverter);
-            var actual = JsonConvert.SerializeObject(reopenedDicomFile, jsonDicomConverter);
+            var dicomFileFromPathJson = JsonConvert.SerializeObject(dicomFileFromPath.Dataset, jsonDicomConverter);
+            var dicomFileFromStreamJson = JsonConvert.SerializeObject(dicomFileFromStream.Dataset, jsonDicomConverter);
+            var actual = JsonConvert.SerializeObject(reopenedDicomFile.Dataset, jsonDicomConverter);
 
-            Assert.Equal(expected, actual);
+            Assert.Equal(dicomFileFromPathJson, dicomFileFromStreamJson);
+            Assert.Equal(actual, dicomFileFromStreamJson);
         }
     }
 }
