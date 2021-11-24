391e48711d024a442471b5ea4dea94a6195d7abc
Alexander Moerman <alexander.moerman@gmail.com>
2021-11-24 20:02:27 +0100

    Revert "Remove TempFileBuffer, it is never used"
    As you might have guessed, it IS used. :-)
    
    This reverts commit 75e9f5f1486abf3d82bbae5395336479c0a9e84d.

---
+++ FO-DICOM.Core/IO/Buffer/TempFileBuffer.cs
@@ -0,0 +1,123 @@
+﻿// Copyright (c) 2012-2021 fo-dicom contributors.
+// Licensed under the Microsoft Public License (MS-PL).
+
+using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FellowOakDicom.IO.Buffer
+{
+    /// <summary>
+    /// Temporary file-based byte buffer.
+    /// </summary>
+    public sealed class TempFileBuffer : IByteBuffer
+    {
+        #region FIELDS
+
+        private readonly IFileReference _file;
+
+        #endregion
+
+        #region CONSTRUCTORS
+
+        /// <summary>
+        /// Initializes a <see cref="TempFileBuffer"/> object.
+        /// </summary>
+        /// <param name="data">Byte array subject to buffering.</param>
+        public TempFileBuffer(byte[] data)
+        {
+            _file = TemporaryFile.Create();
+            Size = data.Length;
+
+            using var stream = _file.OpenWrite();
+            stream.Write(data, 0, (int)Size);
+        }
+
+        #endregion
+
+        #region PROPERTIES
+
+        /// <summary>
+        /// Gets whether data is buffered in memory or not.
+        /// </summary>
+        public bool IsMemory => false;
+
+        /// <summary>
+        /// Gets the size of the buffered data.
+        /// </summary>
+        public long Size { get; }
+
+        /// <summary>
+        /// Gets the data.
+        /// </summary>
+        public byte[] Data
+        {
+            get
+            {
+                var size = (int)Size;
+                var data = new byte[size];
+                GetByteRange(0, size, data);
+                return data;
+            }
+        }
+
+        #endregion
+
+        #region METHODS
+
+        /// <inheritdoc />
+        public void GetByteRange(long offset, int count, byte[] output)
+        {
+            if (output == null)
+            {
+                throw new ArgumentNullException(nameof(output));
+            }
+            if (output.Length < count)
+            {
+                throw new ArgumentException($"Output array with {output.Length} bytes cannot fit {count} bytes of data");
+            }   
+            
+            using var fs = _file.OpenRead();
+            
+            fs.Seek(offset, SeekOrigin.Begin);
+            fs.Read(output, 0, count);
+        }
+
+        public void CopyToStream(Stream stream)
+        {
+            if (stream == null)
+            {
+                throw new ArgumentNullException(nameof(stream));
+            }
+
+            if (!stream.CanWrite)
+            {
+                throw new InvalidOperationException("Cannot copy to non-writable stream");
+            }
+
+            using var fs = _file.OpenRead();
+
+            fs.CopyTo(stream);
+        }
+
+        public async Task CopyToStreamAsync(Stream stream, CancellationToken cancellationToken)
+        {
+            if (stream == null)
+            {
+                throw new ArgumentNullException(nameof(stream));
+            }
+
+            if (!stream.CanWrite)
+            {
+                throw new InvalidOperationException("Cannot copy to non-writable stream");
+            }
+
+            using var fs = _file.OpenRead();
+
+            await fs.CopyToAsync(stream);
+        }
+
+        #endregion
+    }
+}
