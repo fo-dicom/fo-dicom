52fe3fd5799a639c998b6cfc04651188fb3e874d
Reinhard Gruber <r.gruber@smartinmedia.com>
2021-12-12 17:40:57 +0100

    Add unittests for the interpolation Algorithms and enhanced their behavior

--- FO-DICOM.Core/Imaging/Algorithms/BilinearInterpolation.cs
+++ FO-DICOM.Core/Imaging/Algorithms/BilinearInterpolation.cs
@@ -6,8 +6,22 @@ using System.Threading.Tasks;
 namespace FellowOakDicom.Imaging.Algorithms
 {
 
+    /// <summary>
+    /// 2D interpolation methods using the bilinear algorithm.
+    /// </summary>
     public static class BilinearInterpolation
     {
+
+        /// <summary>
+        /// Rescale 2D 8-bit "grayscale" image using bilinear interpolation.
+        /// </summary>
+        /// <param name="input">Input 8-bit 2D "grayscale" image grid.</param>
+        /// <param name="inputWidth">Width of input grid.</param>
+        /// <param name="inputHeight">Height of input grid.</param>
+        /// <param name="outputWidth">Requested width of output grid.</param>
+        /// <param name="outputHeight">Requested height of output grid.</param>
+        /// <returns>Bilinear interpolated grid with output <paramref name="outputWidth">width</paramref>
+        /// and <paramref name="outputHeight">height</paramref>.</returns>
         public static byte[] RescaleGrayscale(
             byte[] input,
             int inputWidth,
@@ -17,8 +31,8 @@ namespace FellowOakDicom.Imaging.Algorithms
         {
             byte[] output = new byte[outputWidth * outputHeight];
 
-            double xF = (double)inputWidth / (double)outputWidth;
-            double yF = (double)inputHeight / (double)outputHeight;
+            double xF = ((double)inputWidth - 1) / (outputWidth - 1);
+            double yF = ((double)inputHeight - 1) / (outputHeight - 1);
 
             int xMax = inputWidth - 1;
             int yMax = inputHeight - 1;
@@ -62,6 +76,7 @@ namespace FellowOakDicom.Imaging.Algorithms
             return output;
         }
 
+
         public static short[] RescaleGrayscale(
             short[] input,
             int inputWidth,
@@ -71,8 +86,8 @@ namespace FellowOakDicom.Imaging.Algorithms
         {
             short[] output = new short[outputWidth * outputHeight];
 
-            double xF = (double)inputWidth / (double)outputWidth;
-            double yF = (double)inputHeight / (double)outputHeight;
+            double xF = ((double)inputWidth - 1) / (outputWidth - 1);
+            double yF = ((double)inputHeight - 1) / (outputHeight - 1);
 
             int xMax = inputWidth - 1;
             int yMax = inputHeight - 1;
@@ -116,6 +131,7 @@ namespace FellowOakDicom.Imaging.Algorithms
             return output;
         }
 
+
         public static ushort[] RescaleGrayscale(
             ushort[] input,
             int inputWidth,
@@ -125,8 +141,8 @@ namespace FellowOakDicom.Imaging.Algorithms
         {
             ushort[] output = new ushort[outputWidth * outputHeight];
 
-            double xF = (double)inputWidth / (double)outputWidth;
-            double yF = (double)inputHeight / (double)outputHeight;
+            double xF = ((double)inputWidth - 1) / (outputWidth - 1);
+            double yF = ((double)inputHeight - 1) / (outputHeight - 1);
 
             int xMax = inputWidth - 1;
             int yMax = inputHeight - 1;
@@ -170,6 +186,7 @@ namespace FellowOakDicom.Imaging.Algorithms
             return output;
         }
 
+
         public static int[] RescaleGrayscale(
             int[] input,
             int inputWidth,
@@ -179,8 +196,8 @@ namespace FellowOakDicom.Imaging.Algorithms
         {
             int[] output = new int[outputWidth * outputHeight];
 
-            double xF = (double)inputWidth / (double)outputWidth;
-            double yF = (double)inputHeight / (double)outputHeight;
+            double xF = ((double)inputWidth - 1) / (outputWidth - 1);
+            double yF = ((double)inputHeight - 1) / (outputHeight - 1);
 
             int xMax = inputWidth - 1;
             int yMax = inputHeight - 1;
@@ -224,6 +241,7 @@ namespace FellowOakDicom.Imaging.Algorithms
             return output;
         }
 
+
         public static uint[] RescaleGrayscale(
             uint[] input,
             int inputWidth,
@@ -233,8 +251,8 @@ namespace FellowOakDicom.Imaging.Algorithms
         {
             uint[] output = new uint[outputWidth * outputHeight];
 
-            double xF = (double)inputWidth / (double)outputWidth;
-            double yF = (double)inputHeight / (double)outputHeight;
+            double xF = ((double)inputWidth - 1) / (outputWidth - 1);
+            double yF = ((double)inputHeight - 1) / (outputHeight - 1);
 
             int xMax = inputWidth - 1;
             int yMax = inputHeight - 1;
@@ -278,6 +296,7 @@ namespace FellowOakDicom.Imaging.Algorithms
             return output;
         }
 
+
         public static byte[] RescaleColor24(
             byte[] input,
             int inputWidth,
@@ -287,8 +306,8 @@ namespace FellowOakDicom.Imaging.Algorithms
         {
             byte[] output = new byte[outputWidth * outputHeight * 3];
 
-            double xF = (double)inputWidth / (double)outputWidth;
-            double yF = (double)inputHeight / (double)outputHeight;
+            double xF = ((double)inputWidth - 1) / (outputWidth - 1);
+            double yF = ((double)inputHeight - 1) / (outputHeight - 1);
 
             int xMax = inputWidth - 1;
             int yMax = inputHeight - 1;
@@ -320,8 +339,8 @@ namespace FellowOakDicom.Imaging.Algorithms
                         dx1 = ox0 - ox1;
                         dx2 = 1.0 - dx1;
 
-                        int x1 = ox1 * 3; //first byte of first pixel
-                        int x2 = ox2 * 3; //first byte of second pixel
+                        int x1 = ox1 * 3; // first byte of first pixel
+                        int x2 = ox2 * 3; // first byte of second pixel
 
                         output[yo0 + px] =
                             (byte)
@@ -344,8 +363,6 @@ namespace FellowOakDicom.Imaging.Algorithms
                             ((dy2 * ((dx2 * input[yo1 + x1]) + (dx1 * input[yo1 + x2])))
                                 + (dy1 * ((dx2 * input[yo2 + x1]) + (dx1 * input[yo2 + x2]))));
                         px++;
-                        x1++;
-                        x2++;
                     }
                 }
                 );
@@ -353,6 +370,7 @@ namespace FellowOakDicom.Imaging.Algorithms
 
             return output;
         }
+
 
         public static byte[] RescaleColor32(
             byte[] input,
@@ -363,8 +381,8 @@ namespace FellowOakDicom.Imaging.Algorithms
         {
             byte[] output = new byte[outputWidth * outputHeight * 4];
 
-            double xF = (double)inputWidth / (double)outputWidth;
-            double yF = (double)inputHeight / (double)outputHeight;
+            double xF = ((double)inputWidth - 1) / (outputWidth - 1);
+            double yF = ((double)inputHeight - 1) / (outputHeight - 1);
 
             int xMax = inputWidth - 1;
             int yMax = inputHeight - 1;
@@ -425,8 +443,6 @@ namespace FellowOakDicom.Imaging.Algorithms
                             ((dy2 * ((dx2 * input[yo1 + ox1]) + (dx1 * input[yo1 + ox2])))
                                 + (dy1 * ((dx2 * input[yo2 + ox1]) + (dx1 * input[yo2 + ox2]))));
                         px++;
-                        ox1++;
-                        ox2++;
                     }
                 }
                 );
@@ -434,5 +450,7 @@ namespace FellowOakDicom.Imaging.Algorithms
 
             return output;
         }
+
+
     }
 }
