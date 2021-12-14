11ddf8a606794eae460e6cd4767d1ed3694163cd
Alexander Moerman <alexander.moerman@gmail.com>
2021-12-14 14:35:19 +0100

    Set server benchmarks hardcoded for a little while

--- Tests/FO-DICOM.Benchmark/Program.cs
+++ Tests/FO-DICOM.Benchmark/Program.cs
@@ -1,16 +1,19 @@
 ﻿// Copyright (c) 2012-2021 fo-dicom contributors.
 // Licensed under the Microsoft Public License (MS-PL).
 
+using System.Threading.Tasks;
 using BenchmarkDotNet.Running;
 
 namespace FellowOakDicom.Benchmark
 {
     static class Program
     {
-        static void Main()
+        static async Task Main()
         {
             // Run all benchmarks in assembly
-            BenchmarkRunner.Run(typeof(Program).Assembly);
+            BenchmarkRunner.Run<ServerBenchmarks>();
+
+
         }
     }
 }
