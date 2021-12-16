faa6305d1322429c14dad96ca37c0e650cde47a3
Alexander Moerman <alexander.moerman@gmail.com>
2021-12-16 17:28:31 +0100

    Add memory provider

---
+++ FO-DICOM.Core/Memory/ArrayPoolMemory.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Toolkit.HighPerformance.Buffers;
+using System;
+
+namespace FellowOakDicom.Memory
+{
+    public class ArrayPoolMemory : IMemory
+    {
+        private readonly MemoryOwner<byte> _memoryOwner;
+
+        public ArrayPoolMemory(MemoryOwner<byte> memoryOwner)
+        {
+            _memoryOwner = memoryOwner ?? throw new ArgumentNullException(nameof(memoryOwner));
+            Bytes = _memoryOwner.DangerousGetArray().Array;
+        }
+
+        ~ArrayPoolMemory() => Dispose();
+
+        public int Length => _memoryOwner.Length;
+        public byte[] Bytes { get; }
+        public Span<byte> Span => _memoryOwner.Span;
+        public Memory<byte> Memory => _memoryOwner.Memory;
+        
+        public void Dispose() => _memoryOwner.Dispose();
+    }
+}
