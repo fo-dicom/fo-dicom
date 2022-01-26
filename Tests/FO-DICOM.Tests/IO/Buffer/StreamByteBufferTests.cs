381758f183986f780ae4fbe667660dd64efb19a0
Josiah Vinson <jovinson@microsoft.com>
2022-01-25 23:19:55 -0800

    Adding MemoryStream tests

--- Tests/FO-DICOM.Tests/IO/Buffer/StreamByteBufferTests.cs
+++ Tests/FO-DICOM.Tests/IO/Buffer/StreamByteBufferTests.cs
@@ -58,10 +58,102 @@ namespace FellowOakDicom.Tests.IO.Buffer
         #region Unit tests
 
         [Theory]
+        [InlineData(0, 255, 20, 150)]
+        [InlineData(0, 255, 0, 128)]
+        [InlineData(0, 255, 5, 1)]
+        [InlineData(0, 255, 30, 224)]
+        [InlineData(0, 255, 12, 0)]
+        [InlineData(50, 100, 20, 15)]
+        [InlineData(50, 100, 5, 1)]
+        [InlineData(50, 100, 12, 0)]
+        public void GetByteRange_CompareWithInitializer_ExactMatch(int ctorOffset, int ctorCount, int byteRangeOffset, int byteRangeCount)
+        {
+            // Arrange
+            var bytes = Enumerable.Range(0, 255).Select(i => (byte)i).ToArray();
+            using var ms = new MemoryStream(bytes);
+            var buffer = new StreamByteBuffer(ms, ctorOffset, ctorCount);
+            var expected = new ArraySegment<byte>(bytes, ctorOffset + byteRangeOffset, Math.Min(ctorCount, byteRangeCount));
+
+            // Act
+            var actual = new byte[Math.Min(ctorCount, byteRangeCount)];
+            buffer.GetByteRange(byteRangeOffset, byteRangeCount, actual);
+
+            // Assert
+            Assert.Equal(expected.Count, actual.Length);
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void GetByteRange_WhenBufferIsSmallerThanRequestedCount_Throws()
+        {
+            // Arrange
+            var bytes = Enumerable.Range(0, 255).Select(i => (byte)i).ToArray();
+            using var ms = new MemoryStream(bytes);
+            var streamByteBuffer = new StreamByteBuffer(ms, 0, 255);
+
+            // Act + Assert
+            var buffer = new byte[10];
+            Assert.Throws<ArgumentException>(() => streamByteBuffer.GetByteRange(0, 20, buffer));
+        }
+
+        [Theory]
+        [InlineData(255, 20, 150)]
+        [InlineData(255, 0, 128)]
+        [InlineData(255, 5, 1)]
+        [InlineData(255, 30, 224)]
+        [InlineData(255, 12, 0)]
+        [InlineData(5 * (1024 * 1024) + 256, 0, 5 * (1024 * 1024) + 256)]
+        [InlineData(5 * (1024 * 1024) + 256, 0, 5 * (1024 * 1024))]
+        [InlineData(5 * (1024 * 1024) + 256, 256, 5 * (1024 * 1024))]
+        public void CopyToStream_ShouldWorkCorrectly(int total, int offset, int count)
+        {
+            // Arrange
+            var bytes = Enumerable.Range(0, total).Select(i => (byte)i).ToArray();
+            using var inputMs = new MemoryStream(bytes);
+            using var outputMs = new MemoryStream(bytes.Length);
+            var buffer = new StreamByteBuffer(inputMs, offset, count);
+            var expected = new ArraySegment<byte>(bytes, offset, count);
+
+            // Act
+            buffer.CopyToStream(outputMs);
+
+            // Assert
+            var actual = outputMs.ToArray();
+            Assert.Equal(expected.Count, actual.Length);
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData(255, 20, 150)]
+        [InlineData(255, 0, 128)]
+        [InlineData(255, 5, 1)]
+        [InlineData(255, 30, 224)]
+        [InlineData(255, 12, 0)]
+        [InlineData(5 * (1024 * 1024) + 256, 0, 5 * (1024 * 1024) + 256)]
+        [InlineData(5 * (1024 * 1024) + 256, 0, 5 * (1024 * 1024))]
+        [InlineData(5 * (1024 * 1024) + 256, 256, 5 * (1024 * 1024))]
+        public async Task CopyToStreamAsync_ShouldWorkCorrectly(int total, int offset, int count)
+        {
+            // Arrange
+            var bytes = Enumerable.Range(0, total).Select(i => (byte)i).ToArray();
+            using var inputMs = new MemoryStream(bytes);
+            using var outputMs = new MemoryStream(bytes.Length);
+            var buffer = new StreamByteBuffer(inputMs, offset, count);
+            var expected = new ArraySegment<byte>(bytes, offset, count);
+
+            // Act
+            await buffer.CopyToStreamAsync(outputMs, CancellationToken.None);
+
+            // Assert
+            var actual = outputMs.ToArray();
+            Assert.Equal(expected.Count, actual.Length);
+            Assert.Equal(expected, actual);
+        }
+        [Theory]
         [InlineData(0, 255)]
         [InlineData(0, 10)]
         [InlineData(10, 245)]
-        public void Data_CompareWithInitializer_ExactMatch(int offset, int count)
+        public void DataFromBufferedStream_CompareWithInitializer_ExactMatch(int offset, int count)
         {
             // Arrange
             var bytes = Enumerable.Range(0, 255).Select(i => (byte)i).ToArray();
@@ -86,7 +178,7 @@ namespace FellowOakDicom.Tests.IO.Buffer
         [InlineData(50, 100, 20, 15)]
         [InlineData(50, 100, 5, 1)]
         [InlineData(50, 100, 12, 0)]
-        public void GetByteRange_CompareWithInitializer_ExactMatch(int ctorOffset, int ctorCount, int byteRangeOffset, int byteRangeCount)
+        public void GetByteRangeFromBufferedStream_CompareWithInitializer_ExactMatch(int ctorOffset, int ctorCount, int byteRangeOffset, int byteRangeCount)
         {
             // Arrange
             var bytes = Enumerable.Range(0, 255).Select(i => (byte)i).ToArray();
@@ -104,7 +196,7 @@ namespace FellowOakDicom.Tests.IO.Buffer
         }
 
         [Fact]
-        public void GetByteRange_WhenBufferIsSmallerThanRequestedCount_Throws()
+        public void GetByteRangeFromBufferedStream_WhenBufferIsSmallerThanRequestedCount_Throws()
         {
             // Arrange
             var bytes = Enumerable.Range(0, 255).Select(i => (byte)i).ToArray();
@@ -125,7 +217,7 @@ namespace FellowOakDicom.Tests.IO.Buffer
         [InlineData(5 * (1024*1024) + 256, 0, 5 * (1024*1024) + 256)]
         [InlineData(5 * (1024*1024) + 256, 0, 5 * (1024*1024))]
         [InlineData(5 * (1024*1024) + 256, 256, 5 * (1024*1024))]
-        public void CopyToStream_ShouldWorkCorrectly(int total, int offset, int count)
+        public void CopyToStreamFromBufferedStream_ShouldWorkCorrectly(int total, int offset, int count)
         {
             // Arrange
             var bytes = Enumerable.Range(0, total).Select(i => (byte)i).ToArray();
@@ -152,7 +244,7 @@ namespace FellowOakDicom.Tests.IO.Buffer
         [InlineData(5 * (1024*1024) + 256, 0, 5 * (1024*1024) + 256)]
         [InlineData(5 * (1024*1024) + 256, 0, 5 * (1024*1024))]
         [InlineData(5 * (1024*1024) + 256, 256, 5 * (1024*1024))]
-        public async Task CopyToStreamAsync_ShouldWorkCorrectly(int total, int offset, int count)
+        public async Task CopyToStreamAsyncFromBufferedStream_ShouldWorkCorrectly(int total, int offset, int count)
         {
             // Arrange
             var bytes = Enumerable.Range(0, total).Select(i => (byte)i).ToArray();
