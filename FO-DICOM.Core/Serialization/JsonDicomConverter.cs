1bbb286d80ef3babb0c51e19defb7d94221f4d8e
Shiva N Kumar <v-shivakumar@microsoft.com>
2022-09-15 14:14:17 -0400

    check only for autovalidate flag

--- FO-DICOM.Core/Serialization/JsonDicomConverter.cs
+++ FO-DICOM.Core/Serialization/JsonDicomConverter.cs
@@ -887,7 +887,7 @@ namespace FellowOakDicom.Serialization
                 {
                     childValues.Add(parsed);
                 }
-                else if (reader.TokenType == JsonTokenType.String && (_numberSerializationMode == NumberSerializationMode.AsString || _numberSerializationMode == NumberSerializationMode.PreferablyAsNumber))
+                else if (reader.TokenType == JsonTokenType.String && !_autoValidate)
                 {
                     hasNonNumericString = true;
                     childValues.Add(reader.GetString());
