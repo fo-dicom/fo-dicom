59dd0e97b971bc79dc8675a889ef55014c0f60dd
Alexander Moerman <alexander.moerman@gmail.com>
2022-09-19 17:20:19 +0200

    Remove native codecs from DICOM.Dump
     This could never work because the codecs have their own private copy of all the core code

--- Tools/FO-DICOM.Dump/MainWindow.xaml.cs
+++ Tools/FO-DICOM.Dump/MainWindow.xaml.cs
@@ -2,24 +2,13 @@
 // Licensed under the Microsoft Public License (MS-PL).
 
 using FellowOakDicom.Imaging;
-using FellowOakDicom.Imaging.NativeCodec;
 using FellowOakDicom.IO.Buffer;
 using Microsoft.Win32;
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace FellowOakDicom.Dump
 {
@@ -43,7 +32,6 @@ namespace FellowOakDicom.Dump
             new DicomSetupBuilder()
                 .RegisterServices(s => s
                     .AddFellowOakDicom()
-                    .AddTranscoderManager<NativeTranscoderManager>()
                     .AddImageManager<ImageSharpImageManager>())
                 .Build();
         }
