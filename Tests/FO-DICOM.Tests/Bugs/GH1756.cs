4b6f4afac4b08b49a3f040728f7bbaa5c65ef3a1
Alexander Moerman <alexander.moerman@gmail.com>
2024-03-13 10:51:31 +0100

    Add tests for functional shared groups

--- Tests/FO-DICOM.Tests/Bugs/GH1756.cs
+++ Tests/FO-DICOM.Tests/Bugs/GH1756.cs
@@ -6,16 +6,21 @@ namespace FellowOakDicom.Tests.Bugs
     [Collection(TestCollections.Imaging)]
     public class GH1756
     {
-        [Fact]
-        public void GrayScaleRenderOptionsFromWindowCenterAndWidthEqualToNaNShouldFallbackCorrectly()
+        [Theory]
+        [InlineData("NaN", "NaN")]
+        [InlineData("100", "NaN")]
+        [InlineData("NaN", "100")]
+        [InlineData("0.99", "100")]
+        [InlineData("0", "100")]
+        [InlineData("-1", "100")]
+        public void GrayScaleRenderOptionsFromInvalidWindowCenterOrWidthShouldFallbackCorrectly(string windowWidth, string windowCenter)
         {
             // Arrange
             var dicomFile = DicomFile.Open("./Test Data/CT-MONO2-16-ankle");
             
-            // Set value of <dicomTagName> to NaN
             dicomFile.Dataset.ValidateItems = false;
-            dicomFile.Dataset.AddOrUpdate(DicomTag.WindowWidth, "NaN");
-            dicomFile.Dataset.AddOrUpdate(DicomTag.WindowCenter, "NaN");
+            dicomFile.Dataset.AddOrUpdate(DicomTag.WindowWidth, windowWidth);
+            dicomFile.Dataset.AddOrUpdate(DicomTag.WindowCenter, windowCenter);
             
             // Act
             var grayScaleRenderOptions = GrayscaleRenderOptions.FromDataset(dicomFile.Dataset, 0);
@@ -25,33 +30,27 @@ namespace FellowOakDicom.Tests.Bugs
             Assert.Equal(4048, grayScaleRenderOptions.WindowWidth);
         }
         
-        [Fact]
-        public void GrayScaleRenderOptionsFromWindowWidthEqualToNaNShouldFallbackCorrectly()
+        [Theory]
+        [InlineData("NaN", "NaN")]
+        [InlineData("100", "NaN")]
+        [InlineData("NaN", "100")]
+        [InlineData("0.99", "100")]
+        [InlineData("0", "100")]
+        [InlineData("-1", "100")]
+        public void GrayScaleRenderOptionsFromInvalidWindowCenterOrWidthInFunctionalGroupShouldFallbackCorrectly(string windowWidth, string windowCenter)
         {
             // Arrange
             var dicomFile = DicomFile.Open("./Test Data/CT-MONO2-16-ankle");
             
-            // Set value of <dicomTagName> to NaN
             dicomFile.Dataset.ValidateItems = false;
-            dicomFile.Dataset.AddOrUpdate(DicomTag.WindowWidth, "NaN");
-            
-            // Act
-            var grayScaleRenderOptions = GrayscaleRenderOptions.FromDataset(dicomFile.Dataset, 0);
-
-            // Assert
-            Assert.Equal(1032, grayScaleRenderOptions.WindowCenter);
-            Assert.Equal(4048, grayScaleRenderOptions.WindowWidth);
-        }
-        
-        [Fact]
-        public void GrayScaleRenderOptionsFromWindowCenterEqualToNaNShouldFallbackCorrectly()
-        {
-            // Arrange
-            var dicomFile = DicomFile.Open("./Test Data/CT-MONO2-16-ankle");
-            
-            // Set value of <dicomTagName> to NaN
-            dicomFile.Dataset.ValidateItems = false;
-            dicomFile.Dataset.AddOrUpdate(DicomTag.WindowCenter, "NaN");
+            dicomFile.Dataset.Remove(DicomTag.WindowWidth);
+            dicomFile.Dataset.Remove(DicomTag.WindowCenter);
+            var referencedImageSequence = new DicomDataset { ValidateItems = false };
+            referencedImageSequence.AddOrUpdate(DicomTag.WindowWidth, windowWidth); 
+            referencedImageSequence.AddOrUpdate(DicomTag.WindowCenter, windowCenter); 
+            var sharedFunctionalGroups = new DicomDataset { ValidateItems = false };
+            sharedFunctionalGroups.AddOrUpdate(new DicomSequence(DicomTag.ReferencedImageSequence, referencedImageSequence));
+            dicomFile.Dataset.AddOrUpdate(new DicomSequence(DicomTag.SharedFunctionalGroupsSequence, sharedFunctionalGroups));
             
             // Act
             var grayScaleRenderOptions = GrayscaleRenderOptions.FromDataset(dicomFile.Dataset, 0);
