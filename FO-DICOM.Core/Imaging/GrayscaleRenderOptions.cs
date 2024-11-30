dde516b8eb1cc3130e8bcb68b5bd832b57c5db72
Jaime Olivares <jolivares@efferenthealth.com>
2024-11-30 10:13:57 -0500

    Refined VOILUTFunction GetValue

--- FO-DICOM.Core/Imaging/GrayscaleRenderOptions.cs
+++ FO-DICOM.Core/Imaging/GrayscaleRenderOptions.cs
@@ -189,10 +189,15 @@ namespace FellowOakDicom.Imaging
                 return null;
             }
 
-            bool hasLut = dataset.TryGetValue(DicomTag.VOILUTFunction, 0, out string voiLutFunction);
             var functional = dataset.FunctionalGroupValues(frame);
-            bool hasLutF = functional.TryGetValue(DicomTag.VOILUTFunction, 0, out string voiLutFunctionF);
             var bits = BitDepth.FromDataset(dataset);
+
+            // #1891 VOI LUT Function with empty value causes a crash
+            if (!functional.TryGetValue(DicomTag.VOILUTFunction, 0, out string voiLutFunction)
+            && !dataset.TryGetValue(DicomTag.VOILUTFunction, 0, out voiLutFunction))
+            {
+                voiLutFunction = "LINEAR";
+            }
 
             var options = new GrayscaleRenderOptions(bits)
             {
@@ -209,14 +214,7 @@ namespace FellowOakDicom.Imaging
 
                 WindowWidth = windowWidth,
                 WindowCenter = windowCenter,
-
-                // #1891 VOI LUT Function with empty value causes a crash
-                VOILUTFunction = hasLut
-                    ? voiLutFunction
-                    : (hasLutF
-                    ? voiLutFunctionF
-                    : "LINEAR"),
-
+                VOILUTFunction = voiLutFunction,
                 ColorMap = GetColorMap(dataset)
             };
 
@@ -255,6 +253,13 @@ namespace FellowOakDicom.Imaging
                 return null;
             }
 
+            // #1891 VOI LUT Function with empty value causes a crash
+            if (!functional.TryGetValue(DicomTag.VOILUTFunction, 0, out string voiLutFunction)
+            && !dataset.TryGetValue(DicomTag.VOILUTFunction, 0, out voiLutFunction))
+            {
+                voiLutFunction = "LINEAR";
+            }
+
             var bits = BitDepth.FromDataset(dataset);
             var options = new GrayscaleRenderOptions(bits)
             {
@@ -271,12 +276,7 @@ namespace FellowOakDicom.Imaging
 
                 WindowWidth = windowWidth,
                 WindowCenter = windowCenter,
-
-                VOILUTFunction = dataset.Contains(DicomTag.VOILUTFunction)
-                    ? dataset.GetSingleValue<string>(DicomTag.VOILUTFunction)
-                    : functional.Contains(DicomTag.VOILUTFunction)
-                    ? functional.GetSingleValue<string>(DicomTag.VOILUTFunction)
-                    : "LINEAR",
+                VOILUTFunction = voiLutFunction,
                 ColorMap = GetColorMap(dataset)
             };
 
