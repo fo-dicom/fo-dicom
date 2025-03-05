7cc99a23a526913d0fe8cefa2c3930ef092177ae
Reinhard Gruber <gofal@users.noreply.github.com>
2025-03-05 11:55:14 +0100

    unittest to reproduce the issue

---
+++ Tests/FO-DICOM.Tests/Bugs/GH1941.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) 2012-2025 fo-dicom contributors.
+// Licensed under the Microsoft Public License (MS-PL).
+#nullable disable
+
+using FellowOakDicom.Imaging;
+using Xunit;
+
+namespace FellowOakDicom.Tests.Bugs
+{
+    public class GH1941
+    {
+        [Fact]
+        public void DicomFile_ContainingVOILUTFunctionWithEmptyValue_GrayscaleRenderOptionsMustPass()
+        {
+            var file = DicomFile.Open(TestData.Resolve("GH1941.dcm"));
+            Assert.True(file.Dataset.Contains(DicomTag.BitsStored));
+
+            var dicomImage = new DicomImage(file.Dataset);
+            IImage image = dicomImage.RenderImage(0); // It threw an exception at this point
+            Assert.NotNull(image);
+        }
+    }
+}
