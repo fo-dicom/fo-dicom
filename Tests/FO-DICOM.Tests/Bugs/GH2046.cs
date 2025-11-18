65ca292466700e6118a2cd85579745d62a1effcb
James A Sutherland <j@sutherland.pw>
2025-11-18 10:28:20 -0600

    Fix typo: Xunit.Abstracts → Xunit.Abstractions

--- Tests/FO-DICOM.Tests/Bugs/GH2046.cs
+++ Tests/FO-DICOM.Tests/Bugs/GH2046.cs
@@ -12,7 +12,7 @@ using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Xunit;
-using Xunit.Abstracts;
+using Xunit.Abstractions;
 
 namespace FellowOakDicom.Tests.Bugs
 {
