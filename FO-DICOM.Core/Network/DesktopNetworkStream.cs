17cbbfe167aa4dffafbbd6d9ae76353da20ee6b9
James A Sutherland <j@sutherland.pw>
2025-11-20 17:45:23 -0600

    Revert SO_REUSEADDR change causing PlatformNotSupportedException
    The SO_REUSEADDR socket option was causing PlatformNotSupportedException
    on some platforms. Reverting this change as the port exhaustion issue
    may have been caused by missing server listening waits rather than
    needing socket options.
    
    The real fixes for race conditions are:
    - AsyncTestHelper.WaitForServerListeningAsync() usage
    - Proper awaiting of SendAsync() which already handles completion

--- FO-DICOM.Core/Network/DesktopNetworkStream.cs
+++ FO-DICOM.Core/Network/DesktopNetworkStream.cs
@@ -46,7 +46,6 @@ namespace FellowOakDicom.Network
             {
                 NoDelay = options.NoDelay
             };
-            _tcpClient.Client.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
             if (options.ReceiveBufferSize.HasValue)
             {
                 _tcpClient.ReceiveBufferSize = options.ReceiveBufferSize.Value;
