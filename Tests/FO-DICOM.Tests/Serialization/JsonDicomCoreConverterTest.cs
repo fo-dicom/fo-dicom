360b2088fc73322a50f14e1df12e95774268bc67
James A Sutherland <j@sutherland.pw>
2025-11-24 12:03:53 -0600

    Fix test issues: avoid TOCTOU in dictionary benchmark and use NET9+ X509 API
    - JsonDicomConverterTest: Build dictionary once outside timing loop
    - JsonDicomCoreConverterTest: Build dictionary once outside timing loop
    - DicomClientTlsTest: Use X509CertificateLoader.LoadPkcs12FromFile on NET9+

--- Tests/FO-DICOM.Tests/Serialization/JsonDicomCoreConverterTest.cs
+++ Tests/FO-DICOM.Tests/Serialization/JsonDicomCoreConverterTest.cs
@@ -174,10 +174,10 @@ namespace FellowOakDicom.Tests.Serialization
                 }
             });
 
+            var dict = DicomDictionary.Default.ToDictionary(dde => dde.Keyword, dde => dde.Tag);
             var millisecondsPerCallC = TimeCall(100, () =>
             {
-                var dict = DicomDictionary.Default.ToDictionary(dde => dde.Keyword, dde => dde.Tag);
-                foreach (var kw in DicomDictionary.Default.Select(dde => dde.Keyword))
+                foreach (var kw in dict.Keys)
                 {
                     var tag = dict[kw];
                     Assert.NotNull(tag);
