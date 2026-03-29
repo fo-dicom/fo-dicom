0a64bf802389741ad3b9e42c9c4163cd0b0cb4c7
Reinhard Gruber <gofal@users.noreply.github.com>
2026-03-29 18:58:27 +0200

    update copyright/year in test projects

--- Documentation/Startup.cs
+++ Documentation/Startup.cs
@@ -1,3 +1,6 @@
+// Copyright (c) 2012-2026 fo-dicom contributors.
+// Licensed under the Microsoft Public License (MS-PL).
+
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
