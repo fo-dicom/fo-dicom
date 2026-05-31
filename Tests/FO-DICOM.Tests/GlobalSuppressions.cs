1db36911936f2240c93e0786836eed833ad021f8
Reinhard Gruber <gofal@users.noreply.github.com>
2026-05-31 22:49:58 +0200

    remove the waring to avoid formatted strings in logging-messages from test-project, because the simplicity of the call is more importnant than performance in the test projects

---
+++ Tests/FO-DICOM.Tests/GlobalSuppressions.cs
@@ -0,0 +1,9 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Usage", "CA2254:Vorlage muss ein statischer Ausdruck sein", Scope = "namespaceanddescendants", Target = "FellowOakDicom.Tests")]
+[assembly: SuppressMessage("Usage", "CA1873:Avoid potentially expensive logging", Scope = "namespaceanddescendants", Target = "FellowOakDicom.Tests")]
